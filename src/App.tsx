01bc0cfc0b439099a5f5ce99f83653daf2231440
Kelian Baert <kelian@baert.me>
2020-07-28 18:53:59 +0200

    Linting / formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { registerRootComponent } from 'expo';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {StatusBar} from "expo-status-bar";
+import {registerRootComponent} from "expo";
+import React from "react";
+import {SafeAreaProvider} from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 
 function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+    const isLoadingComplete = useCachedResources();
+    const colorScheme = useColorScheme();
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+    if (!isLoadingComplete) {
+        return null;
+    } else {
+        return (
+            <SafeAreaProvider>
+                <Navigation colorScheme={colorScheme} />
+                <StatusBar />
+            </SafeAreaProvider>
+        );
+    }
 }
 
 export default registerRootComponent(App);
