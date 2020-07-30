177b6bfb66a725afe3cd3ece5fba442e52d822d1
Kelian Baert <kelian@baert.me>
2020-07-30 09:44:51 +0200

    Registered to store on the App root

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import {SafeAreaProvider} from "react-native-safe-area-context";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import store from './state/store';
+import { Provider } from "react-redux";
 
 function App() {
     const isLoadingComplete = useCachedResources();
@@ -14,11 +16,18 @@ function App() {
     if (!isLoadingComplete) {
         return null;
     } else {
+        /* Original code from cli, replaced to add redux
         return (
             <SafeAreaProvider>
                 <Navigation colorScheme={colorScheme} />
                 <StatusBar />
             </SafeAreaProvider>
+        );*/
+        return (
+            <Provider store={store}>
+                <Navigation colorScheme={colorScheme} />
+                <StatusBar />
+            </Provider>
         );
     }
 }
