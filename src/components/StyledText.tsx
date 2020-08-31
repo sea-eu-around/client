d0afa27da493be010437086cf76e1cd455d8eb37
Kelian Baert <kelian@baert.me>
2020-08-31 14:58:14 +0200

    refactor(components): removed some boilerplate code

--- src/components/StyledText.tsx
+++ src/components/StyledText.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import {Text, TextProps} from "./Themed";
+import {Text, TextProps} from "react-native";
 
 export function MonoText(props: TextProps): JSX.Element {
     return <Text {...props} style={[props.style, {fontFamily: "space-mono"}]} />;
