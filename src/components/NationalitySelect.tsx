1e749655ee1fbb486ca0029a21bce67d9c3e9478
Kelian Baert <kelian@baert.me>
2020-09-16 17:21:37 +0200

    feature(profile): improved styling

--- src/components/NationalitySelect.tsx
+++ src/components/NationalitySelect.tsx
@@ -3,21 +3,42 @@ import * as React from "react";
 import {Text, View} from "react-native";
 import CountryPicker, {Country, CountryCode, TranslationLanguageCode} from "react-native-country-picker-modal";
 import i18n from "i18n-js";
+import {connect, ConnectedProps} from "react-redux";
+import {AppState} from "../state/types";
+import themes from "../constants/themes";
 
-export type NationalitySelectProps = {
+// Map props from store
+const mapStateToProps = (state: AppState) => ({
+    theme: themes[state.theming.theme],
+});
+const reduxConnector = connect(mapStateToProps);
+
+// Component props
+export type NationalitySelectProps = ConnectedProps<typeof reduxConnector> & {
     nationality: CountryCode;
     onSelect?: (countryCode: CountryCode) => void;
 };
 
-export class NationalitySelect extends React.Component<NationalitySelectProps> {
+const styles = {
+    containerButton: {
+        marginTop: 4,
+        paddingVertical: 8,
+        paddingHorizontal: 8,
+        borderRadius: 4,
+    },
+};
+
+class NationalitySelect extends React.Component<NationalitySelectProps> {
     render(): JSX.Element {
+        const {theme} = this.props;
+
         return (
             <View style={{flex: 1, width: "100%"}}>
                 <Text>{i18n.t("nationality")}</Text>
                 <CountryPicker
                     countryCode={this.props.nationality}
                     filterProps={{placeholder: i18n.t("search")}}
-                    containerButtonStyle={{}}
+                    containerButtonStyle={[styles.containerButton, {backgroundColor: theme.accentSlight}]}
                     withFlag={true}
                     withFilter={true}
                     withEmoji={false}
@@ -32,3 +53,5 @@ export class NationalitySelect extends React.Component<NationalitySelectProps> {
         );
     }
 }
+
+export default reduxConnector(NationalitySelect);
