c44637d439d244bd46fbeb97df9a2f03c8ff691a
Kelian Baert <kelian@baert.me>
2020-09-16 15:15:03 +0200

    feature(profile): added profile model

---
+++ src/constants/profile-constants.ts
@@ -0,0 +1,46 @@
+import {NumberLiteralType} from "typescript";
+
+/**
+ * University education fields as specified by the International Standard Classification of Education (ISCED), 2013.
+ * @see {@link https://ec.europa.eu/eurostat/statistics-explained/index.php/International_Standard_Classification_of_Education_(ISCED)}
+ * See translations for the actual name of each field.
+ */
+export const EDUCATION_FIELDS = [
+    "field-00",
+    "field-01",
+    "field-02",
+    "field-03",
+    "field-04",
+    "field-05",
+    "field-06",
+    "field-07",
+    "field-08",
+    "field-09",
+    "field-10",
+];
+
+export const LEVELS_OF_STUDY = ["L1", "L2", "L3", "M1", "M2", "PhD"];
+
+export const ROLES = ["student", "staff"];
+export type Role = typeof ROLES[number];
+
+export const STAFF_ROLES = ["teaching", "researcher", "supporting", "administrative", "technical", "ambassador"];
+export type StaffRole = typeof STAFF_ROLES[number];
+
+export const GENDERS = ["M", "F"];
+export type Gender = typeof GENDERS[number];
+
+export const HOBBIES = [
+    "3D printing",
+    "Acrobatics",
+    "Acting",
+    "Amateur radio",
+    "Animation",
+    "Aquascaping",
+    "Astrology",
+    "Astronomy",
+    "Baking",
+    "Baton twirling",
+    "Blogging",
+];
+export type Hobby = typeof HOBBIES[number];
---
+++ src/model/profile.ts
@@ -0,0 +1,16 @@
+import {Role, StaffRole, Gender, Hobby} from "../constants/profile-constants";
+import {CountryCode} from "./country-codes";
+
+export interface FullProfile {
+    firstName: string;
+    lastName: string;
+    email: string;
+    levelOfStudy: number;
+    nationality: CountryCode;
+    role: Role;
+    staffRole: StaffRole;
+    birthDate: number;
+    gender: Gender;
+    hobbies: Hobby[];
+    // languages
+}
