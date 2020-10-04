630368f1de2eaa6ee582b857bb20101819ffd0c4
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-10-04 10:26:45 +0200

    feature: added new actions related to authentication

--- src/state/types.ts
+++ src/state/types.ts
@@ -1,12 +1,22 @@
-import {ThunkAction} from "redux-thunk";
-import {Action} from "redux";
+import {ThunkAction, ThunkDispatch} from "redux-thunk";
+import {Action, AnyAction} from "redux";
 import {Theme} from "../types";
 import {FullProfile} from "../model/profile";
+import {ResponseLoginUser, ResponseRegisterUser} from "../api/response-types";
+import {AuthToken} from "../model/auth-token";
 
 export type AuthState = {
     authenticated: boolean;
-    token: string;
-    connecting: false;
+    validated: boolean;
+    token: null | AuthToken;
+    connecting: boolean;
+    registerEmail: string;
+    registerFailure: boolean;
+    registerErrors: string[];
+    registerSuccess: boolean;
+    loginErrors: string[];
+    verificationToken: string | null; // TODO temporary
+    onboarded: boolean;
 };
 
 export type ThemingState = {
@@ -26,29 +36,67 @@ export type AppState = {
 // Shortcut type for redux-thunk actions (async actions)
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppState, unknown, Action<string>>;
 
+// Shortcut type for redux-thunk dispatch (cast dispatch function to this for async actions)
+export type MyThunkDispatch = ThunkDispatch<AppState, void, AnyAction>;
+
 /*### AUTH ###*/
 
-export type LogInRequestAction = {
+export type RegisterBeginAction = {
     type: string;
-    username: string;
+    email: string;
+    password: string;
+};
+
+export type RegisterSuccessAction = {
+    type: string;
+    user: ResponseRegisterUser;
+};
+
+export type RegisterFailureAction = {
+    type: string;
+    errors: string[];
+};
+
+export type LogInBeginAction = {
+    type: string;
+    email: string;
     password: string;
 };
 
 export type LogInSuccessAction = {
     type: string;
-    token: string;
+    token: AuthToken;
+    user: ResponseLoginUser;
 };
 
 export type LogInFailureAction = {
     type: string;
-    reason: string;
+    errors: string[];
 };
 
 export type LogOutAction = {
     type: string;
 };
 
-export type AuthAction = LogInRequestAction | LogInSuccessAction | LogInFailureAction | LogOutAction;
+export type ValidateAccountSuccessAction = {
+    type: string;
+};
+
+export type ValidateAccountFailureAction = {
+    type: string;
+    errors: string[];
+};
+
+export type AuthAction =
+    | RegisterBeginAction
+    | RegisterSuccessAction
+    | RegisterFailureAction
+    | LogInBeginAction
+    | LogInSuccessAction
+    | LogInFailureAction
+    | LogOutAction
+    | ValidateAccountSuccessAction
+    | ValidateAccountFailureAction;
 
 /*### THEMING ###*/
 
