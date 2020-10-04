e30646165ccc25e0b7d6fd723cb3002677b36af0
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-10-04 10:28:01 +0200

    cleaned-up

--- src/state/profile/reducer.ts
+++ src/state/profile/reducer.ts
@@ -1,26 +1,30 @@
-import {RoleToggle} from "../../components/RoleToggle";
-import {ProfileState, ProfileAction} from "../types";
+import {ProfileState, ProfileAction, AuthAction, SetProfileFieldsAction} from "../types";
 import {PROFILE_ACTION_TYPES} from "./actions";
 
 export const initialState: ProfileState = {
     userProfile: {
         firstName: "Fred",
         lastName: "Roger",
-        email: "test@email.com",
+        email: "fred.roger@univ-brest.fr",
         levelOfStudy: 2,
         nationality: "FR",
         role: "staff",
         gender: "M",
         birthDate: new Date(1999, 6, 2),
         hobbies: ["Acrobatics", "Acting", "Animation"],
+        avatarUri: "",
     },
 };
 
-export const profileReducer = (state: ProfileState = initialState, action: ProfileAction): ProfileState => {
+export const profileReducer = (
+    state: ProfileState = initialState,
+    action: ProfileAction | AuthAction,
+): ProfileState => {
     const newState: ProfileState = {...state}; // shallow copy
     switch (action.type) {
         case PROFILE_ACTION_TYPES.PROFILE_SET_FIELDS:
-            return {...newState, userProfile: {...state.userProfile, ...action.fields}};
+            const {fields} = <SetProfileFieldsAction>action;
+            return {...newState, userProfile: {...state.userProfile, ...fields}};
         default:
             return state;
     }
