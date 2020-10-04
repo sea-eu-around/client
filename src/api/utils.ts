9ab7ef9f6c4efd90da611fe6a3f5e481e3cdf1e9
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-10-04 10:36:01 +0200

    chore: documentation

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,3 +1,8 @@
+/**
+ * Encode arguments for an HTTP request (e.g. ?param1=value1&param2=value2)
+ * @param args - A map that contains argument keys and associated values.
+ * @returns the given arguments, formatted into a HTTP request suffix.
+ */
 export function encodeRequestArguments(args: {[key: string]: string | number | boolean}): string {
     const keys = Object.keys(args);
     if (keys.length == 0) return "";
