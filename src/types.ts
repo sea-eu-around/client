614c18e046f86575a61986ae5f9d875ff6f56a0b
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-10-04 10:23:48 +0200

    refactor: moved navigation-related types

--- src/types.ts
+++ src/types.ts
@@ -6,36 +6,3 @@ export type ThemeValues = typeof themes.light;
 export type FormProps<T> = {
     onSuccessfulSubmit?: (values: T) => void;
 };
-
-export type RootNavigatorScreens = {
-    MainScreen: undefined;
-    LoginScreen: undefined;
-    NotFoundScreen: undefined;
-};
-
-export type LoginTabNavigatorScreens = {
-    LoginForm: undefined;
-    ForgotPassword: undefined;
-} & RootNavigatorScreens;
-
-export type SignupTabNavigatorScreens = {
-    SignupForm: undefined;
-};
-
-export type MainNavigatorTabs = {
-    TabOne: undefined;
-    TabTwo: undefined;
-    TabProfile: undefined;
-};
-
-export type TabOneParamList = {
-    TabOneScreen: undefined;
-};
-
-export type TabTwoParamList = {
-    TabTwoScreen: undefined;
-};
-
-export type TabProfileParamList = {
-    TabProfileScreen: undefined;
-};
