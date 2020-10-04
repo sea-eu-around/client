3e023426644286e02bb3b62175c21dafddab8ac7
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-10-04 10:29:44 +0200

    feature: added new authentication actions

--- src/state/auth/actions.ts
+++ src/state/auth/actions.ts
@@ -1,42 +1,166 @@
-import {LogOutAction, LogInRequestAction, LogInSuccessAction, LogInFailureAction, AppState, AppThunk} from "../types";
-import {ThunkAction} from "redux-thunk";
-import {Action} from "redux";
+import {
+    LogOutAction,
+    LogInBeginAction,
+    LogInSuccessAction,
+    LogInFailureAction,
+    AppThunk,
+    RegisterBeginAction,
+    RegisterSuccessAction,
+    RegisterFailureAction,
+    ValidateAccountSuccessAction,
+    ValidateAccountFailureAction,
+} from "../types";
+import {BACKEND_URL} from "../../constants/config";
+import {AuthToken} from "../../model/auth-token";
+import {ResponseLoginUser, ResponseRegisterUser} from "../../api/response-types";
+import {encodeRequestArguments} from "../../api/utils";
 
 export enum AUTH_ACTION_TYPES {
-    LOG_IN_REQUEST = "AUTH/LOG_IN_REQUEST",
+    REGISTER_BEGIN = "AUTH/REGISTER_BEGIN",
+    REGISTER_SUCCESS = "AUTH/REGISTER_SUCCESS",
+    REGISTER_FAILURE = "AUTH/REGISTER_FAILURE",
+    LOG_IN_BEGIN = "AUTH/LOG_IN_BEGIN",
     LOG_IN_SUCCESS = "AUTH/LOG_IN_SUCCESS",
     LOG_IN_FAILURE = "AUTH/LOG_IN_FAILURE",
     LOG_OUT = "AUTH/LOG_OUT",
+    VALIDATE_ACCOUNT = "AUTH/VALIDATE_ACCOUNT",
+    VALIDATE_ACCOUNT_SUCCESS = "AUTH/VALIDATE_ACCOUNT_SUCCESS",
+    VALIDATE_ACCOUNT_FAILURE = "AUTH/VALIDATE_ACCOUNT_FAILURE",
 }
 
-// Log in actions
+// Register actions
 
-export const requestLogin = (username: string, password: string): LogInRequestAction => ({
-    type: AUTH_ACTION_TYPES.LOG_IN_REQUEST,
-    username,
+export const registerBegin = (email: string, password: string): RegisterBeginAction => ({
+    type: AUTH_ACTION_TYPES.REGISTER_BEGIN,
+    email,
     password,
 });
 
-export const loginSuccess = (token: string): LogInSuccessAction => ({
+// Redux-thunk asynchronous action creator
+export const requestRegister = (email: string, password: string): AppThunk => async (dispatch) => {
+    dispatch(registerBegin(email, password));
+    try {
+        const response = await fetch(`${BACKEND_URL}/auth/register`, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({email, password}),
+        });
+
+        const json = await response.json();
+        console.log(json);
+
+        if (json.error) {
+            dispatch(registerFailure(json.message || [json.error]));
+        } else {
+            dispatch(registerSuccess(json as ResponseRegisterUser));
+        }
+    } catch (error) {
+        console.error(error);
+        dispatch(registerFailure([]));
+    }
+};
+
+export const registerSuccess = (user: ResponseRegisterUser): RegisterSuccessAction => ({
+    type: AUTH_ACTION_TYPES.REGISTER_SUCCESS,
+    user,
+});
+
+export const registerFailure = (errors: string[]): RegisterFailureAction => ({
+    type: AUTH_ACTION_TYPES.REGISTER_FAILURE,
+    errors,
+});
+
+// Log in actions
+
+export const loginBegin = (email: string, password: string): LogInBeginAction => ({
+    type: AUTH_ACTION_TYPES.LOG_IN_BEGIN,
+    email,
+    password,
+});
+
+export const loginSuccess = (token: AuthToken, user: ResponseLoginUser): LogInSuccessAction => ({
     type: AUTH_ACTION_TYPES.LOG_IN_SUCCESS,
     token,
+    user,
 });
 
-export const loginFailure = (reason: string): LogInFailureAction => ({
+export const loginFailure = (errors: string[]): LogInFailureAction => ({
     type: AUTH_ACTION_TYPES.LOG_IN_FAILURE,
-    reason,
+    errors,
 });
 
-// Example asynchronous action creator (redux-thunk)
-export const login = (username: string, password: string): AppThunk => async (dispatch) => {
-    /* example:
-    const resp = await loginAPI()
-    dispatch(
-        loginSuccess({token})
-    )
-    */
+export const requestLogin = (email: string, password: string): AppThunk => async (dispatch) => {
+    dispatch(loginBegin(email, password));
+    try {
+        const response = await fetch(`${BACKEND_URL}/auth/login`, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({email, password}),
+        });
+
+        const json = await response.json();
+        console.log(json);
+
+        if (json.error) {
+            dispatch(loginFailure(typeof json.message === "string" ? [json.message] : json.message || [json.error]));
+        } else {
+            dispatch(loginSuccess(json.token, json.user as ResponseLoginUser));
+        }
+    } catch (error) {
+        console.error(error);
+        dispatch(loginFailure([]));
+    }
 };
 
 export const logout = (): LogOutAction => ({
     type: AUTH_ACTION_TYPES.LOG_OUT,
+});
+
+// Account validation actions
+
+export const requestValidateAccount = (validationToken: string, email: string): AppThunk => async (dispatch) => {
+    try {
+        // TODO remove the email from here
+        const response = await fetch(
+            `${BACKEND_URL}/auth/verify${encodeRequestArguments({email, token: validationToken})}`,
+            {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+                /*body: JSON.stringify({
+                    validationToken,
+                    email,
+                }),*/
+            },
+        );
+
+        const json = await response.json();
+        console.log(json);
+
+        if (json.error) {
+            dispatch(validateAccountFailure(json.message || [json.error]));
+        } else {
+            dispatch(validateAccountSuccess());
+        }
+    } catch (error) {
+        console.error(error);
+        dispatch(validateAccountFailure([]));
+    }
+};
+
+export const validateAccountSuccess = (): ValidateAccountSuccessAction => ({
+    type: AUTH_ACTION_TYPES.VALIDATE_ACCOUNT_SUCCESS,
+});
+
+export const validateAccountFailure = (errors: string[]): ValidateAccountFailureAction => ({
+    type: AUTH_ACTION_TYPES.VALIDATE_ACCOUNT_FAILURE,
+    errors,
 });
--- src/state/auth/reducer.ts
+++ src/state/auth/reducer.ts
@@ -1,25 +1,89 @@
-import {AuthState, AuthAction, LogInSuccessAction} from "../types";
+import {
+    AuthState,
+    AuthAction,
+    LogInSuccessAction,
+    RegisterFailureAction,
+    LogInFailureAction,
+    RegisterBeginAction,
+    RegisterSuccessAction,
+} from "../types";
 import {AUTH_ACTION_TYPES} from "./actions";
 
 export const initialState: AuthState = {
     authenticated: false,
-    token: "",
+    token: null,
     connecting: false,
+    validated: false,
+    verificationToken: null, // TODO temporary
+    onboarded: false,
+    registerEmail: "",
+    registerSuccess: false,
+    registerFailure: false,
+    registerErrors: [],
+    loginErrors: [],
 };
 
-export const authReducer = (state: AuthState = initialState, action: AuthAction) => {
+export const authReducer = (state: AuthState = initialState, action: AuthAction): AuthState => {
     const newState: AuthState = {...state}; // shallow copy
 
     switch (action.type) {
-        case AUTH_ACTION_TYPES.LOG_IN_REQUEST:
-            return {...newState, connecting: true};
-        case AUTH_ACTION_TYPES.LOG_IN_FAILURE:
-            return {...newState, connecting: false};
-        case AUTH_ACTION_TYPES.LOG_IN_SUCCESS:
-            const {token} = <LogInSuccessAction>action;
-            return {...newState, connecting: false, authenticated: true, token};
-        case AUTH_ACTION_TYPES.LOG_OUT:
-            return {...newState, token: "", authenticated: false};
+        case AUTH_ACTION_TYPES.REGISTER_BEGIN: {
+            const {email} = <RegisterBeginAction>action;
+            return {
+                ...newState,
+                registerFailure: false,
+                registerErrors: [],
+                registerSuccess: false,
+                registerEmail: email,
+            };
+        }
+        case AUTH_ACTION_TYPES.REGISTER_FAILURE: {
+            const {errors} = <RegisterFailureAction>action;
+            return {...newState, registerFailure: true, registerErrors: errors, registerSuccess: false};
+        }
+        case AUTH_ACTION_TYPES.REGISTER_SUCCESS: {
+            const {
+                user: {verificationToken, onboarded},
+            } = <RegisterSuccessAction>action;
+            return {
+                ...newState,
+                registerFailure: false,
+                registerErrors: [],
+                registerSuccess: true,
+                verificationToken,
+                onboarded,
+            };
+        }
+        case AUTH_ACTION_TYPES.VALIDATE_ACCOUNT_SUCCESS: {
+            return {...newState, validated: true};
+        }
+        case AUTH_ACTION_TYPES.VALIDATE_ACCOUNT_FAILURE: {
+            return {...newState, validated: false};
+        }
+        case AUTH_ACTION_TYPES.LOG_IN_BEGIN: {
+            return {...newState, connecting: true, loginErrors: []};
+        }
+        case AUTH_ACTION_TYPES.LOG_IN_FAILURE: {
+            const {errors} = <LogInFailureAction>action;
+            return {...newState, connecting: false, loginErrors: errors};
+        }
+        case AUTH_ACTION_TYPES.LOG_IN_SUCCESS: {
+            const {
+                token,
+                user: {onboarded},
+            } = <LogInSuccessAction>action;
+            return {
+                ...newState,
+                connecting: false,
+                authenticated: true,
+                loginErrors: [],
+                token,
+                onboarded,
+            };
+        }
+        case AUTH_ACTION_TYPES.LOG_OUT: {
+            return {...newState, token: null, authenticated: false};
+        }
         default:
             return state;
     }
