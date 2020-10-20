a7382fa9c8689b8c3e2866c696edbeac2f68ee01
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-10-20 11:07:10 +0200

    refactor: added a getter for the default locale

--- src/localization.ts
+++ src/localization.ts
@@ -2,12 +2,20 @@ import * as Localization from "expo-localization";
 import i18n from "i18n-js";
 import translations from "./constants/translations";
 
+export type SupportedLocale = "en" | "fr";
+export const SUPPORTED_LOCALES: string[] = ["en", "fr"];
+
+export function getDefaultLocale(): SupportedLocale {
+    const idx = SUPPORTED_LOCALES.indexOf(Localization.locale);
+    return idx == -1 ? "en" : (SUPPORTED_LOCALES[idx] as SupportedLocale);
+}
+
 export default function configureLocalization(): void {
     // Set the key-value pairs for the supported languages.
     i18n.translations = translations;
 
     // Set the locale on startup
-    i18n.locale = Localization.locale;
+    i18n.locale = getDefaultLocale();
 
     // When a value is missing from a language, fallback to another language with the key present.
     i18n.fallbacks = true;
