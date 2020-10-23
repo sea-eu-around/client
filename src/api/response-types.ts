20a972a27e20fef49aa168bc53a7df558f68f4f6
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-10-23 21:18:42 +0200

    feature: added DTOs for profile creation

--- src/api/response-types.ts
+++ src/api/response-types.ts
@@ -1,3 +1,7 @@
+import {Gender} from "../constants/profile-constants";
+import {CountryCode} from "../model/country-codes";
+import {SpokenLanguage} from "../model/spoken-language";
+
 export type UserDto = {
     role: "USER" | "ADMIN" | "TEACHER" | "STUDENT";
     email: string;
@@ -15,3 +19,22 @@ export type LoginDto = {
     user: UserDto;
     token: TokenDto;
 };
+
+export type CreateProfileDtoCommon = {
+    firstName: string;
+    lastName: string;
+    gender: Gender;
+    birthdate: string;
+    nationality: CountryCode;
+    languages: SpokenLanguage[];
+};
+
+export type CreateProfileDtoStudent = CreateProfileDtoCommon & {
+    degree: string;
+};
+
+export type CreateProfileDtoStaff = CreateProfileDtoCommon & {
+    staffRole: string;
+};
+
+export type CreateProfileDto = CreateProfileDtoStudent | CreateProfileDtoStaff;
