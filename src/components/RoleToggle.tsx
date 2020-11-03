4d282286725a4de8d4bca4e70d7c6cdea1ea1c38
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-11-03 08:56:38 +0100

    improved props handling

--- src/components/RoleToggle.tsx
+++ src/components/RoleToggle.tsx
@@ -2,25 +2,26 @@ import * as React from "react";
 
 import i18n from "i18n-js";
 import {Role, ROLES} from "../constants/profile-constants";
-import {ButtonGroup} from "react-native-elements";
+import {ButtonGroup, ButtonGroupProps} from "react-native-elements";
 
 export type RoleToggleProps = {
     role: Role;
     onSelect?: (role: Role) => void;
-};
+} & Partial<ButtonGroupProps>;
 
 export function RoleToggle(props: RoleToggleProps): JSX.Element {
     const buttonLabels = ROLES.map((r: string) => i18n.t(`roles.${r}`));
-    const {role} = props;
+    const {role, onSelect, ...otherProps} = props;
 
     return (
         <ButtonGroup
             onPress={(idx: number) => {
-                if (props.onSelect) props.onSelect(ROLES[idx]);
+                if (onSelect) onSelect(ROLES[idx]);
             }}
             selectedIndex={ROLES.indexOf(role)}
             buttons={buttonLabels}
             containerStyle={{height: 35, marginLeft: 0, marginRight: 0}}
+            {...otherProps}
         />
     );
 }
