6ae21e4297e8eea1dd6d6c3a68b4cbaedc1f15f5
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-11-10 19:45:50 +0100

    added a middleware to handle navigation for some actions

---
+++ src/state/navigation-middleware.ts
@@ -0,0 +1,29 @@
+import {AnyAction, Middleware, MiddlewareAPI, Dispatch} from "redux";
+import {rootNavigate} from "../navigation/utils";
+import {AppState, AUTH_ACTION_TYPES, PROFILE_ACTION_TYPES} from "./types";
+
+export const navigationMiddleware: Middleware<unknown, AppState> = (store: MiddlewareAPI<Dispatch, AppState>) => (
+    next: Dispatch<AnyAction>,
+) => (action: AnyAction) => {
+    const state: AppState = store.getState();
+
+    // TODO action printing
+    console.log(action.type);
+
+    switch (action.type) {
+        case PROFILE_ACTION_TYPES.PROFILE_CREATE_SUCCESS: {
+            rootNavigate("MainScreen");
+            break;
+        }
+        case AUTH_ACTION_TYPES.LOG_IN_SUCCESS: {
+            rootNavigate(state.auth.onboarded ? "MainScreen" : "OnboardingScreen");
+            break;
+        }
+        case AUTH_ACTION_TYPES.REGISTER_SUCCESS: {
+            rootNavigate("ValidationEmailSentScreen");
+            break;
+        }
+    }
+
+    next(action);
+};
