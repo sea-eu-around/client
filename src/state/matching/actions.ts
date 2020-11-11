4115e1710c813b48dc6f56678191f1cfb93541db
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-11-11 16:58:33 +0100

    added like/dislike actions

--- src/state/matching/actions.ts
+++ src/state/matching/actions.ts
@@ -1,9 +1,11 @@
+import {requestBackend} from "../../api/utils";
 import {
     SetOfferFilterAction,
     MATCHING_ACTION_TYPES,
     ResetMatchingFiltersAction,
     SetMatchingFiltersAction,
     MatchingFiltersState,
+    AppThunk,
 } from "../types";
 
 export const setOfferFilter = (offerId: string, value: boolean): SetOfferFilterAction => ({
@@ -20,3 +22,11 @@ export const setMatchingFilters = (filters: Partial<MatchingFiltersState>): SetM
 export const resetMatchingFilters = (): ResetMatchingFiltersAction => ({
     type: MATCHING_ACTION_TYPES.RESET_MATCHING_FILTERS,
 });
+
+export const likeProfile = (profileId: string): AppThunk => async () => {
+    await requestBackend("matching/like", "POST", {}, {toProfileId: profileId}, true, true);
+};
+
+export const dislikeProfile = (profileId: string): AppThunk => async () => {
+    await requestBackend("matching/decline", "POST", {}, {toProfileId: profileId}, true, true);
+};
