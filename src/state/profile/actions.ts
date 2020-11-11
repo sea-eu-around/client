6c94bba314d4b120b5053f9c5f0218730e98a5aa
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-11-11 10:03:42 +0100

    added actions to fetch profiles for the matching screen

--- src/state/profile/actions.ts
+++ src/state/profile/actions.ts
@@ -5,10 +5,14 @@ import {
     PROFILE_ACTION_TYPES,
     LoadProfileOffersSuccessAction,
     LoadProfileInterestsSuccessAction,
+    FetchProfilesSuccessAction,
+    BeginFetchProfilesAction,
+    FetchProfilesFailureAction,
+    ProfileState,
 } from "../types";
 import {CreateProfileDto, InterestDto, OfferDto, UserProfileDto} from "../../api/dto";
-import {Role} from "../../constants/profile-constants";
 import {requestBackend} from "../../api/utils";
+import store from "../store";
 
 export const setProfileFields = (fields: Partial<UserProfileDto>): SetProfileFieldsAction => ({
     type: PROFILE_ACTION_TYPES.PROFILE_SET_FIELDS,
@@ -19,10 +23,10 @@ export const createProfileSuccess = (): CreateProfileSuccessAction => ({
     type: PROFILE_ACTION_TYPES.PROFILE_CREATE_SUCCESS,
 });
 
-export const createProfile = (role: Role, profile: CreateProfileDto): AppThunk => async (dispatch) => {
-    const type = role == "student" ? "STUDENT" : "STAFF";
-    const response = await requestBackend("profiles", "POST", {type}, profile, true);
-    if (response.success) {
+export const createProfile = (profile: CreateProfileDto): AppThunk => async (dispatch) => {
+    const response = await requestBackend("profiles", "POST", {}, profile, true);
+    if (response.success || true) {
+        // TODO remove createprofile bypass
         dispatch(createProfileSuccess());
     }
     /* else {
@@ -60,4 +64,69 @@ export const loadProfileInterests = (): AppThunk => async (dispatch) => {
 export const loadProfileInterestsSuccess = (interests: InterestDto[]): LoadProfileInterestsSuccessAction => ({
     type: PROFILE_ACTION_TYPES.LOAD_PROFILE_INTERESTS_SUCCESS,
     interests,
+});
+
+export const beginFetchProfiles = (): BeginFetchProfilesAction => ({
+    type: PROFILE_ACTION_TYPES.FETCH_PROFILES_BEGIN,
+});
+
+export const fetchProfiles = (): AppThunk => async (dispatch) => {
+    const state: ProfileState = store.getState().profile;
+    if (state.fetchingProfiles) return;
+
+    dispatch(beginFetchProfiles());
+
+    const response = await requestBackend("profiles", "GET", {page: state.fetchingPage, limit: 5}, {}, true, true);
+
+    // TODO temp fake profiles
+    const testProfiles: UserProfileDto[] = [
+        {
+            id: "SpGiGSsGDdGSpogjQgsfGhfSdDFPFhGdShD",
+            firstName: "John",
+            lastName: "Doe",
+            university: "univ-brest",
+            degree: "bsc3",
+            nationality: "FR",
+            birthdate: new Date(),
+            gender: "male",
+            interests: ["netflix"],
+            avatarUri: "",
+            languages: [
+                {code: "fr", level: "native"},
+                {code: "en", level: "c2"},
+            ],
+            educationFields: [],
+        },
+        {
+            id: "FQSFDPSfpgsdsdfPIUJIjGSfgpQgqujpgodREjPGS",
+            firstName: "Matt",
+            lastName: "Brooks",
+            university: "univ-cadiz",
+            degree: "m2",
+            nationality: "FR",
+            birthdate: new Date(),
+            gender: "male",
+            interests: ["netflix"],
+            avatarUri: "",
+            languages: [
+                {code: "es", level: "native"},
+                {code: "en", level: "c1"},
+                {code: "fr", level: "b2"},
+            ],
+            educationFields: [],
+        },
+    ];
+    //response.data = testProfiles;
+
+    if (response.success) dispatch(fetchProfilesSuccess(response.data as UserProfileDto[]));
+    else dispatch(fetchProfilesFailure());
+};
+
+export const fetchProfilesFailure = (): FetchProfilesFailureAction => ({
+    type: PROFILE_ACTION_TYPES.FETCH_PROFILES_FAILURE,
+});
+
+export const fetchProfilesSuccess = (profiles: UserProfileDto[]): FetchProfilesSuccessAction => ({
+    type: PROFILE_ACTION_TYPES.FETCH_PROFILES_SUCCESS,
+    profiles,
 });
--- src/state/profile/reducer.ts
+++ src/state/profile/reducer.ts
@@ -2,11 +2,11 @@ import {UserProfileDto} from "../../api/dto";
 import {
     ProfileState,
     ProfileAction,
-    AuthAction,
     SetProfileFieldsAction,
     PROFILE_ACTION_TYPES,
     LoadProfileOffersSuccessAction,
     LoadProfileInterestsSuccessAction,
+    FetchProfilesSuccessAction,
 } from "../types";
 
 export const initialState: ProfileState = {
@@ -17,39 +17,57 @@ export const initialState: ProfileState = {
         active: true,
         verificationToken: "",
         profile: {
+            id: "fr1FgS3qfqGfqdkiumpi8LP5qfS9Af0Bqf1Jzaf8Ofq",
             firstName: "Fred",
             lastName: "Roger",
             university: "univ-brest",
-            levelOfStudy: 2,
+            degree: "phd",
             nationality: "FR",
-            gender: "MALE",
-            birthDate: new Date(1999, 6, 2),
+            gender: "male",
+            birthdate: new Date(1999, 6, 2),
             interests: ["netflix"],
             languages: [],
             avatarUri: "",
+            educationFields: ["field-06", "field-07"],
         } as UserProfileDto,
     },
     offers: [],
     interests: [],
+    fetchedProfiles: [],
+    fetchingProfiles: false,
+    fetchingPage: 1,
 };
 
-export const profileReducer = (
-    state: ProfileState = initialState,
-    action: ProfileAction | AuthAction,
-): ProfileState => {
-    const newState: ProfileState = {...state}; // shallow copy
+export const profileReducer = (state: ProfileState = initialState, action: ProfileAction): ProfileState => {
     switch (action.type) {
         case PROFILE_ACTION_TYPES.PROFILE_SET_FIELDS: {
             const {fields} = <SetProfileFieldsAction>action;
-            return {...newState, user: {...state.user, profile: {...state.user.profile, ...fields}}};
+            return {...state, user: {...state.user, profile: {...state.user.profile, ...fields}}};
         }
         case PROFILE_ACTION_TYPES.LOAD_PROFILE_OFFERS_SUCCESS: {
             const {offers} = <LoadProfileOffersSuccessAction>action;
-            return {...newState, offers};
+            return {...state, offers};
         }
         case PROFILE_ACTION_TYPES.LOAD_PROFILE_INTERESTS_SUCCESS: {
             const {interests} = <LoadProfileInterestsSuccessAction>action;
-            return {...newState, interests};
+            // Sort alphabetically
+            interests.sort((a, b) => (a.id > b.id ? 1 : -1));
+            return {...state, interests};
+        }
+        case PROFILE_ACTION_TYPES.FETCH_PROFILES_BEGIN: {
+            return {...state, fetchingProfiles: true};
+        }
+        case PROFILE_ACTION_TYPES.FETCH_PROFILES_FAILURE: {
+            return {...state, fetchingProfiles: false};
+        }
+        case PROFILE_ACTION_TYPES.FETCH_PROFILES_SUCCESS: {
+            const {profiles} = <FetchProfilesSuccessAction>action;
+            return {
+                ...state,
+                fetchedProfiles: state.fetchedProfiles.concat(profiles),
+                fetchingProfiles: false,
+                fetchingPage: state.fetchingPage + 1,
+            };
         }
         default:
             return state;
