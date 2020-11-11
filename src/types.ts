a0bf433614f8524b9fcba3cdece97164f38bc033
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-11-11 10:06:12 +0100

    updated theme typing

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,11 @@
+import {ThemeProps as RNEThemeProps} from "react-native-elements";
 import themes from "./constants/themes";
 
-export type Theme = "light" | "dark";
-export type ThemeValues = typeof themes.light;
+export type ThemeKey = "light" | "dark";
+export type Theme = typeof themes.light;
 
 export type FormProps<T> = {
     onSuccessfulSubmit?: (values: T) => void;
 };
+
+export type ThemeProps = RNEThemeProps<Theme>;
