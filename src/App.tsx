6004a01c65a4c052e9dfade1e554e392f30a25a6
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-11-12 16:08:27 +0100

    removed warning disabling

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,12 @@ import useCachedResources from "./hooks/useCachedResources";
 import Navigation from "./navigation";
 import {Provider} from "react-redux";
 import configureLocalization from "./localization";
-import {YellowBox} from "react-native";
 import {SafeAreaProvider} from "react-native-safe-area-context";
 import ConnectedThemeProvider from "./components/providers/ConnectedThemeProvider";
 import store from "./state/store";
 
-// YellowBox.ignoreWarnings(["Require cycle"]);
-YellowBox.ignoreWarnings(["VirtualizedLists should never be nested"]); // TODO: Remove when fixed
+// Example for disabling a warning :
+// YellowBox.ignoreWarnings(["VirtualizedLists should never be nested"]);
 
 function App() {
     const isLoadingComplete = useCachedResources();
