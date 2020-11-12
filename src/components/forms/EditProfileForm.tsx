d62230fa64614249f97485d70279de17bf8fa032
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-11-12 14:23:42 +0100

    added support for null user on EditProfileForm

--- src/components/forms/EditProfileForm.tsx
+++ src/components/forms/EditProfileForm.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {KeyboardAvoidingView, Platform, Text, TextStyle, View} from "react-native";
+import {ActivityIndicator, KeyboardAvoidingView, Platform, Text, TextStyle, View} from "react-native";
 import i18n from "i18n-js";
 import {Avatar, withTheme} from "react-native-elements";
 import EducationFieldPicker from "../EducationFieldPicker";
@@ -28,8 +28,8 @@ import {preTheme} from "../../styles/utils";
 
 // Component props
 export type EditProfileFormProps = ThemeProps & {
-    user: UserDto;
-    onFieldChanged?: (fields: Partial<UserProfileDto>) => void;
+    user: UserDto | null;
+    onChange?: (fields: Partial<UserProfileDto>) => void;
 };
 
 function FormFieldSpacer(): JSX.Element {
@@ -37,17 +37,16 @@ function FormFieldSpacer(): JSX.Element {
 }
 
 class EditProfileForm extends React.Component<EditProfileFormProps> {
+    onFieldChanged(fields: Partial<UserProfileDto>): void {
+        if (this.props.onChange !== undefined) this.props.onChange(fields);
+    }
+
     render() {
         const {theme, user} = this.props;
         const styles = themedStyles(theme);
 
-        const profile = user.profile;
-
-        const onFieldChanged = (fields: Partial<UserProfileDto>) => {
-            if (this.props.onFieldChanged !== undefined) this.props.onFieldChanged(fields);
-        };
-
-        // TODO display origin university
+        const avatarTitle = user ? (user.profile.firstName[0] + user.profile.lastName[0]).toUpperCase() : "";
+        const avatarUri = user && user.profile.avatarUri ? {uri: user.profile.avatarUri} : undefined;
 
         const textInputStyleProps = {
             placeholderTextColor: "#222",
@@ -70,171 +69,191 @@ class EditProfileForm extends React.Component<EditProfileFormProps> {
             focusedStyle: Platform.OS === "web" ? ({outlineColor: "transparent"} as TextStyle) : null,
         };
 
+        let profileFieldComponents = <></>;
+
+        if (user) {
+            const profile = user.profile;
+            profileFieldComponents = (
+                <>
+                    <FormRow
+                        label={i18n.t("emailAddress")}
+                        initialValue={user.email}
+                        // validator={VALIDATOR_EMAIL}
+                        display={<Text style={styles.cardText}>{user.email}</Text>}
+                        renderInput={(value: string, error: string | null, onChange: (value: string) => void) => (
+                            <ValidatedTextInput
+                                placeholder={i18n.t("emailAddress")}
+                                value={value}
+                                error={error || undefined}
+                                onChangeText={(val) => onChange(val)}
+                                keyboardType="email-address"
+                                autoCompleteType="email"
+                                autoFocus
+                                {...textInputStyleProps}
+                            />
+                        )}
+                        //apply={(email: string) => onFieldChanged({email})}
+                        locked={true}
+                    />
+                    <FormFieldSpacer />
+                    <FormRow
+                        label={i18n.t("dateOfBirth")}
+                        initialValue={profile.birthdate}
+                        display={<FormattedDate style={styles.cardText} date={profile.birthdate} />}
+                        overrideModal={(hide: () => void) => (
+                            <BirthDatePicker
+                                date={profile.birthdate}
+                                open={true}
+                                onSelect={(birthdate: Date) => this.onFieldChanged({birthdate})}
+                                onHide={hide}
+                            />
+                        )}
+                    />
+                    <FormFieldSpacer />
+                    <FormRow
+                        label={i18n.t("nationality")}
+                        initialValue={profile.nationality}
+                        display={<FormattedNationality style={styles.cardText} countryCode={profile.nationality} />}
+                        overrideModal={(hide: () => void) => (
+                            <NationalityPicker
+                                nationality={profile.nationality}
+                                onSelect={(cc: CountryCode) => this.onFieldChanged({nationality: cc})}
+                                onHide={hide}
+                            />
+                        )}
+                    />
+                    <FormFieldSpacer />
+                    <FormRow
+                        label={i18n.t("gender")}
+                        initialValue={profile.gender}
+                        display={
+                            <GenderToggle
+                                gender={profile.gender}
+                                onSelect={(gender: Gender) => this.onFieldChanged({gender})}
+                            />
+                        }
+                        noModal={true}
+                    />
+                    <FormFieldSpacer />
+                    <FormRow
+                        label={i18n.t("role")}
+                        initialValue={user.role}
+                        display={
+                            <>
+                                <RoleToggle
+                                    role={user.role}
+                                    /*onSelect={(role: Role) => onFieldChanged({role})}*/
+                                    disabled={true}
+                                />
+                                {user.role == "staff" && (
+                                    <StaffRoleToggle
+                                        staffRole={profile.staffRole || null}
+                                        onSelect={(staffRole: StaffRole) => this.onFieldChanged({staffRole})}
+                                    />
+                                )}
+                                {user.role == "student" && (
+                                    <DegreeToggle
+                                        degree={profile.degree}
+                                        onUpdate={(degree?: Degree) => this.onFieldChanged({degree})}
+                                    />
+                                )}
+                            </>
+                        }
+                        noModal={true}
+                    />
+                    <FormFieldSpacer />
+                    <FormRow
+                        label={i18n.t("fieldsOfEducation")}
+                        initialValue={profile.educationFields}
+                        display={<EducationFieldPicker fields={profile.educationFields}></EducationFieldPicker>}
+                        noModal={true}
+                    />
+                    <FormFieldSpacer />
+                    <FormRow
+                        label={i18n.t("interests")}
+                        initialValue={profile.interests}
+                        display={
+                            <InterestsPicker
+                                interests={profile.interests}
+                                onChange={(interests: string[]) => this.onFieldChanged({interests})}
+                            ></InterestsPicker>
+                        }
+                        noModal={true}
+                    />
+                    <FormFieldSpacer />
+                    <FormRow
+                        label={i18n.t("spokenLanguages")}
+                        initialValue={profile.languages}
+                        validator={VALIDATOR_ONBOARDING_LANGUAGES}
+                        display={
+                            <>
+                                {profile.languages.map((l: SpokenLanguageDto, i: number) => (
+                                    <View key={i}>
+                                        <Text>
+                                            {i18n.t(`languageNames.${l.code}`)} ({i18n.t(`languageLevels.${l.level}`)})
+                                        </Text>
+                                    </View>
+                                ))}
+                            </>
+                        }
+                        renderInput={(
+                            value: SpokenLanguageDto[],
+                            error: string | null,
+                            onChange: (value: SpokenLanguageDto[]) => void,
+                        ) => (
+                            <>
+                                <SpokenLanguagesInput
+                                    languages={value}
+                                    onChange={(languages: SpokenLanguageDto[]) => onChange(languages)}
+                                />
+                                <FormFieldSpacer />
+                                <FormFieldSpacer />
+                                <FormFieldSpacer />
+                                <FormFieldSpacer />
+                                <FormFieldSpacer />
+                                <FormFieldSpacer />
+                            </>
+                        )}
+                        apply={(languages: SpokenLanguageDto[]) => this.onFieldChanged({languages})}
+                    />
+                </>
+            );
+        }
+
         return (
             <View style={styles.screenWrapper}>
                 <View style={[styles.topView, {backgroundColor: theme.accent}]}>
                     <Avatar
                         size={120}
                         rounded
-                        title={(profile.firstName[0] + profile.lastName[0]).toUpperCase()}
+                        title={avatarTitle}
                         containerStyle={{backgroundColor: theme.accentSecondary}}
-                        source={profile.avatarUri === "" ? undefined : {uri: profile.avatarUri}}
+                        source={avatarUri}
                     >
-                        <AvatarEditButton
-                            onPictureSelected={(avatarUri: string) => {
-                                onFieldChanged({avatarUri});
-                            }}
-                        />
+                        {user && (
+                            <AvatarEditButton
+                                onPictureSelected={(avatarUri: string) => {
+                                    this.onFieldChanged({avatarUri});
+                                }}
+                            />
+                        )}
                     </Avatar>
-                    <Text style={styles.name}>{profile.firstName + " " + profile.lastName}</Text>
-                    <FormattedUniversity
-                        containerStyle={styles.universityContainer}
-                        style={styles.university}
-                        university={getUniversityFromEmail(user.email)}
-                    ></FormattedUniversity>
+                    <Text style={styles.name}>{user ? user.profile.firstName + " " + user.profile.lastName : ""}</Text>
+                    {user && (
+                        <FormattedUniversity
+                            containerStyle={styles.universityContainer}
+                            style={styles.university}
+                            university={getUniversityFromEmail(user.email)}
+                        ></FormattedUniversity>
+                    )}
                 </View>
                 <ScrollView style={styles.scrollWrapper} keyboardShouldPersistTaps="always">
                     <KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={-100} style={styles.formWrapper}>
                         <View style={styles.titleWrapper}>
                             <Text style={styles.title}>{i18n.t("myProfile")}</Text>
                         </View>
-                        <FormRow
-                            label={i18n.t("emailAddress")}
-                            initialValue={user.email}
-                            // validator={VALIDATOR_EMAIL}
-                            display={<Text style={styles.cardText}>{user.email}</Text>}
-                            renderInput={(value: string, error: string | null, onChange: (value: string) => void) => (
-                                <ValidatedTextInput
-                                    placeholder={i18n.t("emailAddress")}
-                                    value={value}
-                                    error={error || undefined}
-                                    onChangeText={(val) => onChange(val)}
-                                    keyboardType="email-address"
-                                    autoCompleteType="email"
-                                    autoFocus
-                                    {...textInputStyleProps}
-                                />
-                            )}
-                            //apply={(email: string) => onFieldChanged({email})}
-                            locked={true}
-                        />
-                        <FormFieldSpacer />
-                        <FormRow
-                            label={i18n.t("dateOfBirth")}
-                            initialValue={profile.birthdate}
-                            display={<FormattedDate style={styles.cardText} date={profile.birthdate} />}
-                            overrideModal={(hide: () => void) => (
-                                <BirthDatePicker
-                                    date={profile.birthdate}
-                                    open={true}
-                                    onSelect={(birthdate: Date) => onFieldChanged({birthdate})}
-                                    onHide={hide}
-                                />
-                            )}
-                        />
-                        <FormFieldSpacer />
-                        <FormRow
-                            label={i18n.t("nationality")}
-                            initialValue={profile.nationality}
-                            display={<FormattedNationality style={styles.cardText} countryCode={profile.nationality} />}
-                            overrideModal={(hide: () => void) => (
-                                <NationalityPicker
-                                    nationality={profile.nationality}
-                                    onSelect={(countryCode: CountryCode) => onFieldChanged({nationality: countryCode})}
-                                    onHide={hide}
-                                />
-                            )}
-                        />
-                        <FormFieldSpacer />
-                        <FormRow
-                            label={i18n.t("gender")}
-                            initialValue={profile.gender}
-                            display={
-                                <GenderToggle
-                                    gender={profile.gender}
-                                    onSelect={(gender: Gender) => onFieldChanged({gender})}
-                                />
-                            }
-                            noModal={true}
-                        />
-                        <FormFieldSpacer />
-                        <FormRow
-                            label={i18n.t("role")}
-                            initialValue={user.role}
-                            display={
-                                <>
-                                    <RoleToggle
-                                        role={user.role}
-                                        /*onSelect={(role: Role) => onFieldChanged({role})}*/
-                                        disabled={true}
-                                    />
-                                    {user.role == "staff" && (
-                                        <StaffRoleToggle
-                                            staffRole={profile.staffRole || null}
-                                            onSelect={(staffRole: StaffRole) => onFieldChanged({staffRole})}
-                                        />
-                                    )}
-                                    {user.role == "student" && (
-                                        <DegreeToggle
-                                            degree={profile.degree}
-                                            onUpdate={(degree?: Degree) => onFieldChanged({degree})}
-                                        />
-                                    )}
-                                </>
-                            }
-                            noModal={true}
-                        />
-                        <FormFieldSpacer />
-                        <FormRow
-                            label={i18n.t("fieldsOfEducation")}
-                            initialValue={profile.educationFields}
-                            display={<EducationFieldPicker fields={profile.educationFields}></EducationFieldPicker>}
-                            noModal={true}
-                        />
-                        <FormFieldSpacer />
-                        <FormRow
-                            label={i18n.t("interests")}
-                            initialValue={profile.interests}
-                            display={<InterestsPicker interests={profile.interests}></InterestsPicker>}
-                            noModal={true}
-                        />
-                        <FormFieldSpacer />
-                        <FormRow
-                            label={i18n.t("spokenLanguages")}
-                            initialValue={profile.languages}
-                            validator={VALIDATOR_ONBOARDING_LANGUAGES}
-                            display={
-                                <>
-                                    {profile.languages.map((l: SpokenLanguageDto, i: number) => (
-                                        <View key={i}>
-                                            <Text>
-                                                {i18n.t(`languageNames.${l.code}`)} (
-                                                {i18n.t(`languageLevels.${l.level}`)})
-                                            </Text>
-                                        </View>
-                                    ))}
-                                </>
-                            }
-                            renderInput={(
-                                value: SpokenLanguageDto[],
-                                error: string | null,
-                                onChange: (value: SpokenLanguageDto[]) => void,
-                            ) => (
-                                <>
-                                    <SpokenLanguagesInput
-                                        languages={value}
-                                        onChange={(languages: SpokenLanguageDto[]) => onChange(languages)}
-                                    />
-                                    <FormFieldSpacer />
-                                    <FormFieldSpacer />
-                                    <FormFieldSpacer />
-                                    <FormFieldSpacer />
-                                    <FormFieldSpacer />
-                                    <FormFieldSpacer />
-                                </>
-                            )}
-                            apply={(languages: SpokenLanguageDto[]) => onFieldChanged({languages})}
-                        />
+                        {profileFieldComponents}
+                        {!user && <ActivityIndicator size="large" color={theme.accent} />}
                     </KeyboardAvoidingView>
                 </ScrollView>
             </View>
