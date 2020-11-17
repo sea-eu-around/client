d361f737d5b6a1d0300a724d19e652e038da9905
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-11-17 12:27:53 +0100

    cleaned-up onboarding navigator

--- src/navigation/OnboardingNavigator.tsx
+++ src/navigation/OnboardingNavigator.tsx
@@ -6,24 +6,6 @@ import {ONBOARDING_ORDER, ONBOARDING_SCREENS} from "../screens/onboarding";
 const OnboardingStack = createMaterialTopTabNavigator<OnboardingScreens>();
 
 export default function OnboardingNavigator(): JSX.Element {
-    const screens = ONBOARDING_ORDER.map((name: keyof OnboardingScreens, i: number) => {
-        const ComponentClass = ONBOARDING_SCREENS[name];
-        const Wrapper = (props: MaterialTopTabScreenProps<OnboardingScreens>): JSX.Element => (
-            <ComponentClass
-                next={() => {
-                    if (i < ONBOARDING_ORDER.length - 1) props.navigation.navigate(ONBOARDING_ORDER[i + 1]);
-                }}
-                previous={() => {
-                    if (i > 0) props.navigation.navigate(ONBOARDING_ORDER[i - 1]);
-                }}
-                index={i}
-                hasNext={i < ONBOARDING_ORDER.length - 1}
-                {...props}
-            />
-        );
-
-        return <OnboardingStack.Screen key={i} name={name} component={Wrapper} />;
-    });
     return (
         <OnboardingStack.Navigator
             initialRouteName={ONBOARDING_ORDER[0]}
@@ -35,8 +17,31 @@ export default function OnboardingNavigator(): JSX.Element {
                 mass: 5,
             }}
             swipeEnabled={false}
+            lazy={true}
+            lazyPreloadDistance={1}
         >
-            {screens}
+            {ONBOARDING_ORDER.map((name: keyof OnboardingScreens, i: number) => {
+                const ComponentClass = ONBOARDING_SCREENS[name];
+
+                return (
+                    <OnboardingStack.Screen key={i} name={name}>
+                        {(props: MaterialTopTabScreenProps<OnboardingScreens>) => (
+                            <ComponentClass
+                                next={() => {
+                                    if (i < ONBOARDING_ORDER.length - 1)
+                                        props.navigation.navigate(ONBOARDING_ORDER[i + 1]);
+                                }}
+                                previous={() => {
+                                    if (i > 0) props.navigation.navigate(ONBOARDING_ORDER[i - 1]);
+                                }}
+                                index={i}
+                                hasNext={i < ONBOARDING_ORDER.length - 1}
+                                {...props}
+                            />
+                        )}
+                    </OnboardingStack.Screen>
+                );
+            })}
         </OnboardingStack.Navigator>
     );
 }
