c2cf4a7859bd05a9fa27e8ec814f042afb6a5586
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-11-17 09:50:43 +0100

    removed old education fields

--- src/constants/profile-constants.ts
+++ src/constants/profile-constants.ts
@@ -1,35 +1,3 @@
-/**
- * University education fields as specified by the International Standard Classification of Education (ISCED), 2013.
- * @see {@link https://ec.europa.eu/eurostat/statistics-explained/index.php/International_Standard_Classification_of_Education_(ISCED)}
- * See translations for the actual name of each field.
- */
-export const EDUCATION_FIELDS = [
-    "field-00",
-    "field-01",
-    "field-02",
-    "field-03",
-    "field-04",
-    "field-05",
-    "field-06",
-    "field-07",
-    "field-08",
-    "field-09",
-    "field-10",
-    /*
-    "natural-and-physical-science",
-    "information-technology",
-    "engineering-and-related-technology",
-    "architecture-and-building",
-    "agriculture-environnement",
-    "health",
-    "education",
-    "management-and-commerce",
-    "society-and-culture",
-    "creatice-art",
-    "food-and-hospitality",
-    */
-];
-
 export type Degree = "bsc1" | "bsc2" | "bsc3" | "m1" | "m2" | "phd";
 export const DEGREES: Degree[] = ["bsc1", "bsc2", "bsc3", "m1", "m2", "phd"];
 
