97b29b021ef5b7d58c91fd867c466470be0dfe4a
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-11-17 20:29:28 +0100

    added a config entry for avatar compression quality

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,3 +1,10 @@
-export const BACKEND_URL = "http://ec2-35-180-16-83.eu-west-3.compute.amazonaws.com:3000";
 export const APP_VERSION = "0.0.1";
 export const DEBUG_MODE = true;
+
+export const BACKEND_URL = "http://ec2-35-180-16-83.eu-west-3.compute.amazonaws.com:3000";
+
+/**
+ * Specify the quality of compression, from 0 to 1.
+ * 0 means compress for small size, 1 means compress for maximum quality.
+ */
+export const AVATAR_QUALITY = 0.75;
