12c632c6c7ea95254d91f717e4ec28c4936f7470
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-11-22 20:32:56 +0100

    added more information to profile previews

--- src/components/ProfilePreview.tsx
+++ src/components/ProfilePreview.tsx
@@ -21,6 +21,10 @@ import BlockProfileModal from "./modals/BlockProfileModal";
 import {MyThunkDispatch} from "../state/types";
 import {blockProfile} from "../state/matching/actions";
 import store from "../state/store";
+import FormattedUniversity from "./FormattedUniversity";
+import {PARTNER_UNIVERSITIES, University} from "../constants/universities";
+import {OfferValueDto, SpokenLanguageDto} from "../api/dto";
+import {styleTextThin} from "../styles/general";
 
 // Component props
 export type ProfilePreviewProps = ThemeProps & {
@@ -35,6 +39,7 @@ export type ProfilePreviewProps = ThemeProps & {
 // Component state
 export type ProfilePreviewState = {
     expanded: boolean;
+    animating: boolean;
     height: ReAnimated.Value<number>;
     blockModalOpen: boolean;
 };
@@ -48,30 +53,36 @@ class ProfilePreview extends React.Component<ProfilePreviewProps, ProfilePreview
             expanded: false,
             height: new ReAnimated.Value(PROFILE_PREVIEW_COLLAPSED_HEIGHT),
             blockModalOpen: false,
+            animating: false,
         };
         this.layout = {x: 0, y: 0, width: 0, height: 0};
     }
 
     expand() {
-        // Will change fadeAnim value to 1 in 5 seconds
+        const duration = 200;
+        this.setState({...this.state, animating: true});
         ReAnimated.timing(this.state.height, {
             toValue: PROFILE_PREVIEW_EXPANDED_HEIGHT,
-            duration: 200,
+            duration,
             easing: Easing.elastic(1.0),
         }).start();
+        setTimeout(() => this.setState({...this.state, animating: false, expanded: true}), duration);
     }
 
     collapse() {
-        // Will change fadeAnim value to 0 in 5 seconds
+        const duration = 100;
+        this.setState({...this.state, animating: true, expanded: false});
         ReAnimated.timing(this.state.height, {
             toValue: PROFILE_PREVIEW_COLLAPSED_HEIGHT,
-            duration: 100,
+            duration,
             easing: Easing.out(Easing.linear),
         }).start();
+        setTimeout(() => this.setState({...this.state, animating: false}), duration);
     }
 
     hide(onFinish?: () => void) {
         const duration = this.state.expanded ? 160 : 120;
+        this.setState({...this.state, animating: true, expanded: false});
         ReAnimated.timing(this.state.height, {
             toValue: 0,
             duration,
@@ -84,7 +95,6 @@ class ProfilePreview extends React.Component<ProfilePreviewProps, ProfilePreview
     }
 
     toggleExpanded() {
-        this.setState({...this.state, expanded: !this.state.expanded});
         if (!this.state.expanded) {
             this.expand();
             if (this.props.onExpand) this.props.onExpand(this.layout);
@@ -93,10 +103,12 @@ class ProfilePreview extends React.Component<ProfilePreviewProps, ProfilePreview
 
     render() {
         const {theme, profile, style} = this.props;
-        const {expanded, height, blockModalOpen} = this.state;
+        const {expanded, animating, height, blockModalOpen} = this.state;
         const styles = themedStyles(theme);
 
-        // const university = PARTNER_UNIVERSITIES.find((univ: University) => univ.key == profile.university);
+        const university = PARTNER_UNIVERSITIES.find((univ: University) => univ.key == profile.university);
+        const fullName = profile.firstName + " " + profile.lastName;
+        //const fullName = profile.firstName.length % 2 == 0 ? profile.firstName + " " + profile.lastName : "Jimmy Jim McLongname";
 
         return (
             <ReAnimated.View
@@ -116,19 +128,19 @@ class ProfilePreview extends React.Component<ProfilePreviewProps, ProfilePreview
                         if (this.props.onSwipeRight) this.props.onSwipeRight();
                         this.hide();
                     }}
-                    //leftThreshold={400}
-                    //rightThreshold={300}
+                    leftThreshold={100}
+                    rightThreshold={100}
                     renderRightActions={() => (
                         <Animated.View style={[styles.swipeAction, styles.swipeActionRight]}>
                             <View style={[styles.swipeActionContent, styles.swipeActionContentRight]}>
-                                <Text style={styles.swipeActionText}>HIDE</Text>
+                                <Text style={styles.swipeActionText}>{i18n.t("matching.actionHide")}</Text>
                             </View>
                         </Animated.View>
                     )}
                     renderLeftActions={() => (
                         <View style={[styles.swipeAction, styles.swipeActionLeft]}>
                             <View style={[styles.swipeActionContent, styles.swipeActionContentLeft]}>
-                                <Text style={styles.swipeActionText}>MATCH</Text>
+                                <Text style={styles.swipeActionText}>{i18n.t("matching.actionLike")}</Text>
                             </View>
                         </View>
                     )}
@@ -138,31 +150,78 @@ class ProfilePreview extends React.Component<ProfilePreviewProps, ProfilePreview
                         activeOpacity={0.75}
                         style={styles.touchable}
                     >
-                        <View style={styles.avatarContainer}>
-                            <Avatar
-                                size={120}
-                                rounded
-                                title={(profile.firstName[0] + profile.lastName[0]).toUpperCase()}
-                                containerStyle={styles.avatar}
-                                source={
-                                    profile.avatarUrl && profile.avatarUrl !== "" ? {uri: profile.avatarUrl} : undefined
-                                }
-                            ></Avatar>
+                        <View style={styles.collapsedContent}>
+                            <View style={styles.avatarContainer}>
+                                <Avatar
+                                    size={120}
+                                    rounded
+                                    title={(profile.firstName[0] + profile.lastName[0]).toUpperCase()}
+                                    containerStyle={styles.avatar}
+                                    source={
+                                        profile.avatarUrl && profile.avatarUrl !== ""
+                                            ? {uri: profile.avatarUrl}
+                                            : undefined
+                                    }
+                                ></Avatar>
+                            </View>
+                            <View style={styles.infoContainer}>
+                                <Text style={styles.name}>{fullName}</Text>
+                                {university && <FormattedUniversity style={styles.infoText} university={university} />}
+                                <Text style={styles.infoText}>
+                                    {i18n.t(`genders.${profile.gender}`)}
+                                    {", "}
+                                    {i18n.t(`allRoles.${profile.type}`)} (
+                                    {profile.type == "staff"
+                                        ? i18n.t(`staffRoles.${profile.staffRole}`)
+                                        : i18n.t(`degrees.${profile.degree}`)}
+                                    )
+                                </Text>
+                                {/*<Text style={styles.infoText}>{i18n.t(`genders.${profile.gender}`)}</Text>*/}
+                            </View>
                         </View>
-                        <View style={styles.infoContainer}>
-                            <Text style={styles.name}>{profile.firstName + " " + profile.lastName}</Text>
-                            <Text style={styles.infoText}>{i18n.t(`universities.${profile.university}`)}</Text>
-                            <Text style={styles.infoText}>
-                                {i18n.t(`allRoles.${profile.staffRole ? "staff" : "student"}`)} (
-                                {profile.staffRole
-                                    ? i18n.t(`staffRoles.${profile.staffRole}`)
-                                    : i18n.t(`degrees.${profile.degree}`)}
-                                )
-                            </Text>
-                            <Text style={styles.infoText}>{i18n.t(`genders.${profile.gender}`)}</Text>
-                        </View>
-                        {expanded && (
-                            <>
+                        {(expanded || animating) && (
+                            <View style={styles.expandedContent}>
+                                <Text style={styles.expandedSectionTitle}>{i18n.t("spokenLanguages")}</Text>
+                                <View style={styles.chipsContainer}>
+                                    {profile.languages.map((l: SpokenLanguageDto) => (
+                                        <ItemChip
+                                            key={`${profile.id}-${l.code}`}
+                                            text={`${i18n.t(`languageNames.${l.code}`)}${
+                                                l.level != "native" ? ` (${l.level.toUpperCase()})` : ""
+                                            }`}
+                                        />
+                                    ))}
+                                </View>
+                                <Text style={styles.expandedSectionTitle}>{i18n.t("offers")}</Text>
+                                <View style={styles.chipsContainer}>
+                                    {profile.profileOffers.map((offer: OfferValueDto) => (
+                                        <ItemChip
+                                            key={`${profile.id}-${offer.offerId}`}
+                                            text={i18n.t(`allOffers.${offer.offerId}.name`)}
+                                        />
+                                    ))}
+                                </View>
+                                {/*<Text style={styles.expandedSectionTitle}>{i18n.t("fieldsOfEducation")}</Text>
+                                <View style={styles.chipsContainer}>
+                                    {profile.educationFields.map((fieldId: string) => (
+                                        <ItemChip
+                                            key={`${profile.id}-${fieldId}`}
+                                            text={i18n.t(`educationFields.${fieldId}`)}
+                                        />
+                                    ))}
+                                </View>
+                                */}
+                                {/*
+                                <Text style={styles.expandedSectionTitle}>{i18n.t("interests")}</Text>
+                                <View style={styles.chipsContainer}>
+                                    {profile.interests.map((interestId: string) => (
+                                        <ItemChip
+                                            key={`${profile.id}-${interestId}`}
+                                            text={i18n.t(`interests.${interestId}`)}
+                                        />
+                                    ))}
+                                </View>
+                                */}
                                 <TouchableOpacity
                                     style={styles.blockButton}
                                     onPress={() => this.setState({...this.state, blockModalOpen: true})}
@@ -180,7 +239,7 @@ class ProfilePreview extends React.Component<ProfilePreviewProps, ProfilePreview
                                         }}
                                     />
                                 )}
-                            </>
+                            </View>
                         )}
                     </TouchableOpacity>
                 </Swipeable>
@@ -189,41 +248,46 @@ class ProfilePreview extends React.Component<ProfilePreviewProps, ProfilePreview
     }
 }
 
-/*<View style={styles.actionContainer}>
-    <TouchableOpacity style={styles.matchButton}>
-        <Text style={styles.matchButtonText}>MATCH</Text>
-    </TouchableOpacity>
-</View>*/
+const chipStyles = preTheme((theme: Theme) => {
+    return StyleSheet.create({
+        chip: {
+            backgroundColor: theme.accentSlight,
+            borderRadius: 6,
+            //paddingHorizontal: 7,
+            paddingHorizontal: 4,
+            paddingVertical: 2,
+            marginHorizontal: 2,
+            marginVertical: 2,
+            flexGrow: 1,
+            alignItems: "center",
+        },
+        chipText: {
+            fontSize: 14,
+            color: theme.textBlack,
+        },
+    });
+});
 
-/*
+const ItemChip = withTheme(
+    ({text, theme}: {text: string} & ThemeProps): JSX.Element => {
+        const styles = chipStyles(theme);
+        return (
+            <View style={styles.chip}>
+                <Text style={styles.chipText}>{text}</Text>
+            </View>
+        );
+    },
+);
 
-{university && (
-    <FormattedUniversity
-        containerStyle={styles.universityContainer}
-        style={styles.university}
-        university={university}
-    ></FormattedUniversity>
-)}
-
-<InputLabel>{i18n.t("dateOfBirth")}</InputLabel>
-<FormattedDate date={profile.birthdate} />
-
-<Spacer></Spacer>
-<InputLabel>{i18n.t("gender")}</InputLabel>
-<Text>{i18n.t(`genders.${profile.gender}`)}</Text>
-*/
-
-/*
-
-<Spacer></Spacer>
-<InputLabel>{i18n.t("nationality")}</InputLabel>
-<FormattedNationality countryCode={profile.nationality} />
-*/
+export const Separator = withTheme(({theme}: ThemeProps) => {
+    return <View style={themedStyles(theme).separator}></View>;
+});
 
 const sideMargin = 15;
 const verticalSpacing = 10;
-const PROFILE_PREVIEW_COLLAPSED_HEIGHT = 200;
-const PROFILE_PREVIEW_EXPANDED_HEIGHT = 500;
+const cardPadding = 10;
+const PROFILE_PREVIEW_COLLAPSED_HEIGHT = 180;
+const PROFILE_PREVIEW_EXPANDED_HEIGHT = 400;
 
 const themedStyles = preTheme((theme: Theme) => {
     return StyleSheet.create({
@@ -240,13 +304,14 @@ const themedStyles = preTheme((theme: Theme) => {
             width: "100%",
             borderRadius: 10,
             elevation: 1,
-            padding: 10,
+            padding: cardPadding,
+            overflow: "hidden",
             backgroundColor: theme.cardBackground,
         },
         touchable: {
             width: "100%",
             height: "100%",
-            flexDirection: "row",
+            flexDirection: "column",
         },
         swipeAction: {
             width: "100%",
@@ -276,10 +341,25 @@ const themedStyles = preTheme((theme: Theme) => {
         swipeActionText: {
             fontSize: 24,
             letterSpacing: 2,
-            fontFamily: "sans-serif-thin",
+            textTransform: "uppercase",
+            ...styleTextThin,
+        },
+
+        separator: {
+            height: 1,
+            width: "100%",
+            backgroundColor: "#000",
+            opacity: 0.1,
+            alignSelf: "center",
+            marginBottom: 5,
         },
 
         // Card content
+
+        collapsedContent: {
+            flexDirection: "row",
+            height: PROFILE_PREVIEW_COLLAPSED_HEIGHT - verticalSpacing * 2 - cardPadding * 2,
+        },
 
         avatarContainer: {
             justifyContent: "center",
@@ -294,38 +374,48 @@ const themedStyles = preTheme((theme: Theme) => {
             paddingLeft: 10,
         },
         name: {
-            fontSize: 24,
+            fontSize: 22,
             letterSpacing: 0.8,
+            marginBottom: 5,
+            flexShrink: 1, // Ensures text wrapping
             color: theme.text,
         },
         infoText: {
-            fontSize: 18,
+            fontSize: 16,
+            letterSpacing: 0.5,
             color: theme.textLight,
+            flexShrink: 1, // Ensures text wrapping
         },
-        /*university: {
+
+        expandedContent: {
+            flex: 1,
+        },
+
+        chipsContainer: {
+            // TODO clean-up
+            /*flexDirection: "row",
+            flexWrap: "wrap",
+            alignItems: "center",
+            marginTop: 4,*/
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+            //maxHeight: 58,
+            overflow: "hidden",
+        },
+        expandedSectionTitle: {
+            fontFamily: "sans-serif-light",
             fontSize: 14,
+            textTransform: "uppercase",
+            letterSpacing: 1,
+            marginTop: 5,
             color: theme.text,
         },
-        universityContainer: {
-            marginVertical: 5,
-        },*/
         actionContainer: {
             flexDirection: "column",
             flex: 1,
             justifyContent: "flex-end",
         },
-        /*matchButton: {
-            width: "100%",
-            paddingVertical: 10,
-            borderRadius: 2,
-            backgroundColor: theme.accent,
-        },
-        matchButtonText: {
-            fontSize: 20,
-            letterSpacing: 2,
-            textAlign: "center",
-            color: theme.textInverted,
-        },*/
         blockButton: {
             position: "absolute",
             bottom: 0,
