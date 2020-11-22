aeae0c0b8afa63f2cd4942359208ca25b6c2b21e
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-11-22 12:50:02 +0100

    updated linking config

--- src/navigation/linking-config.ts
+++ src/navigation/linking-config.ts
@@ -1,18 +1,18 @@
 import * as Linking from "expo-linking";
 
 export default {
-    prefixes: [Linking.makeUrl("/")],
+    prefixes: [Linking.makeUrl("/"), "https://sea-eu-around.com"],
     config: {
         screens: {
             LoginScreen: {
                 screens: {
-                    login: {
+                    TabSignin: {
                         screens: {
                             LoginForm: "login",
                             ForgotPassword: "forgot-password",
                         },
                     },
-                    signup: {
+                    TabSignup: {
                         screens: {
                             SignupForm: "signup",
                         },
@@ -35,7 +35,8 @@ export default {
                     },
                     TabMessaging: {
                         screens: {
-                            TabMessagingScreen: "messaging",
+                            ChatScreen: "messaging/talk",
+                            MessagingScreen: "messaging",
                         },
                     },
                     TabProfile: {
@@ -50,7 +51,27 @@ export default {
                     },
                 },
             },
+            ValidateEmailSentScreen: "validate/success",
             ValidateEmailScreen: "validate",
+            ChangePassword: "new-password",
+            OnboardingScreen: {
+                screens: {
+                    OnboardingNameScreen: "onboarding/name",
+                    OnboardingPersonalInfoScreen: "onboarding/info",
+                    OnboardingLanguageScreen: "onboarding/language",
+                    OnboardingInterestsScreen: "onboarding/interests",
+                    OnboardingRoleScreen: "onboarding/role/1",
+                    OnboardingRoleSpecificScreen1: "onboarding/role/2",
+                    OnboardingRoleSpecificScreen2: "onboarding/role/3",
+                    OnboardingOffersScreen1: "onboarding/offers/1",
+                    OnboardingOffersScreen2: "onboarding/offers/2",
+                    OnboardingOffersScreen3: "onboarding/offers/3",
+                    OnboardingTosScreen: "onboarding/tos",
+                    OnboardingPrivacyScreen: "onboarding/privacy",
+                },
+            },
+            OnboardingSuccessfulScreen: "onboarding/success",
+            MatchSuccessScreen: "match/success",
             NotFoundScreen: "*",
         },
     },
