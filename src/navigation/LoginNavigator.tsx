2c6fbf44fd06332896344a860ae11de37f39224d
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-11-25 18:36:24 +0100

    cleaned-up

--- src/navigation/LoginNavigator.tsx
+++ src/navigation/LoginNavigator.tsx
@@ -10,29 +10,11 @@ import {screenTitle} from "./utils";
 const SigninStack = createStackNavigator<TabLoginSigninScreens>();
 const Tab = createMaterialTopTabNavigator<TabLoginRoot>();
 
-/*
-function SigninTabNavigator(): JSX.Element {
-    return (
-        <SigninStack.Navigator initialRouteName="LoginForm">
-            <SigninStack.Screen name="LoginForm" component={SubTabLogin} options={{headerShown: false}} />
-            <SigninStack.Screen name="ForgotPassword" component={SubTabForgotPassword} options={{headerShown: false}} />
-        </SigninStack.Navigator>
-    );
-}
-
-function SignupTabNavigator(): JSX.Element {
-    return (
-        <SignupTabStack.Navigator>
-            <SignupTabStack.Screen name="SignupForm" component={TabSignupFormScreen} options={{headerShown: false}} />
-        </SignupTabStack.Navigator>
-    );
-}*/
-
 const LoginNavigator = (): JSX.Element => (
-    <Tab.Navigator initialRouteName="TabSignin" tabBar={(props) => <LoginTabBar {...props} />}>
+    <Tab.Navigator tabBar={(props) => <LoginTabBar {...props} />}>
         <Tab.Screen name="TabSignin">
             {() => (
-                <SigninStack.Navigator initialRouteName="LoginForm">
+                <SigninStack.Navigator>
                     <SigninStack.Screen
                         name="LoginForm"
                         component={SubTabLogin}
