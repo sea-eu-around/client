14410719ceb366e7cc52e7bfc8876cd7281b9ea6
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-12-10 19:39:21 +0100

    improved local images for web

--- src/components/LocalImage.tsx
+++ src/components/LocalImage.tsx
@@ -8,7 +8,8 @@ export type LocalImageProps = Partial<ImageProps> & {imageKey: string};
 class LocalImage extends React.Component<LocalImageProps> {
     render(): JSX.Element {
         const {imageKey, ...otherProps} = this.props;
-        return <Image source={getLocalImage(imageKey, () => this.forceUpdate())} {...otherProps} />;
+        const img = getLocalImage(imageKey, () => this.forceUpdate());
+        return img ? <Image source={img} {...otherProps} /> : <></>;
     }
 }
 
