ec62afac2ea0b191c1ca8302805b8af522a5d31d
Kelian Baert <kelian@baert.me>
2020-12-24 00:27:14 +0100

    improved infinitescroller

--- src/components/InfiniteScroller.tsx
+++ src/components/InfiniteScroller.tsx
@@ -5,8 +5,10 @@ import {
     NativeSyntheticEvent,
     RefreshControl,
     ScrollView,
+    StyleProp,
     StyleSheet,
     View,
+    ViewStyle,
 } from "react-native";
 import {ThemeConsumer} from "react-native-elements";
 import {preTheme} from "../styles/utils";
@@ -23,9 +25,11 @@ export type InfiniteScrollerProps<T> = {
     renderItem: (item: T, hide: () => void) => JSX.Element;
     noResultsComponent: JSX.Element;
     currentPage: number;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    navigation: {isFocused: () => boolean; addListener: (k: any, l: () => void) => void};
+    itemsContainerStyle?: StyleProp<ViewStyle>;
+    progressViewOffset?: number;
 };
-
-type InternalInfiniteScrollerProps<T> = ThemeProps & InfiniteScrollerProps<T>;
 
 // Component state
 type InfiniteScrollerState = {
@@ -34,33 +38,45 @@ type InfiniteScrollerState = {
 
 const SCROLL_DISTANCE_TO_LOAD = 50;
 
-class InfiniteScrollerClass<T> extends React.Component<InternalInfiniteScrollerProps<T>, InfiniteScrollerState> {
+export default class InfiniteScroller<T> extends React.Component<InfiniteScrollerProps<T>, InfiniteScrollerState> {
     scrollViewRef: React.RefObject<ScrollView> = React.createRef<ScrollView>();
 
-    constructor(props: InternalInfiniteScrollerProps<T>) {
+    constructor(props: InfiniteScrollerProps<T>) {
         super(props);
         this.state = {hiddenIds: {}};
     }
 
     fetchMore(): void {
-        const {fetching, fetchMore} = this.props;
-        if (!fetching /*&& navigation.isFocused()*/) fetchMore();
+        const {fetching, fetchMore, navigation} = this.props;
+        if (!fetching && navigation.isFocused()) fetchMore();
     }
 
     hideItem(item: T): void {
-        this.setState({...this.state, hiddenIds: {...this.state.hiddenIds, [this.props.id(item)]: true}});
+        const {items, fetchLimit, id} = this.props;
+        this.setState({...this.state, hiddenIds: {...this.state.hiddenIds, [id(item)]: true}});
+        const shown = items.filter((it) => !this.state.hiddenIds[id(it)]).length;
+        if (shown < fetchLimit) this.fetchMore();
     }
 
     componentDidMount(): void {
-        const numberShown = this.props.items.length - Object.keys(this.state.hiddenIds).length;
-        if (numberShown == 0) this.fetchMore();
+        const {items, navigation, id} = this.props;
+        const shown = items.filter((it) => !this.state.hiddenIds[id(it)]).length;
+        if (shown == 0) this.fetchMore();
+        navigation.addListener("focus", () => this.onFocus());
+        this.onFocus();
+    }
+
+    onFocus(): void {
+        const {items, fetchLimit, fetching} = this.props;
+        if (items.length < fetchLimit && !fetching) this.fetchMore();
     }
 
     componentDidUpdate(oldProps: InfiniteScrollerProps<T>): void {
-        const {fetchLimit, currentPage, id} = this.props;
+        const {fetchLimit, currentPage, id, navigation} = this.props;
 
         const justRefreshed = oldProps.currentPage > 1 && currentPage === 1;
-        if (true /*this.props.navigation.isFocused()*/) {
+
+        if (navigation.isFocused()) {
             const shown = this.props.items.filter((it) => !this.state.hiddenIds[id(it)]).length;
             if (shown < fetchLimit) this.fetchMore();
             // Reset the hidden profiles when the user purposedly refreshes
@@ -69,35 +85,56 @@ class InfiniteScrollerClass<T> extends React.Component<InternalInfiniteScrollerP
     }
 
     render(): JSX.Element {
-        const {theme, items, fetching, refresh, renderItem, noResultsComponent, id} = this.props;
+        const {
+            items,
+            fetching,
+            refresh,
+            renderItem,
+            noResultsComponent,
+            id,
+            itemsContainerStyle,
+            progressViewOffset,
+        } = this.props;
         const {hiddenIds} = this.state;
-        const styles = themedStyles(theme);
 
         return (
-            <ScrollView
-                ref={this.scrollViewRef}
-                style={styles.scroller}
-                refreshControl={<RefreshControl refreshing={fetching} onRefresh={() => refresh()} />}
-                onScroll={(e: NativeSyntheticEvent<NativeScrollEvent>) => {
-                    const {layoutMeasurement, contentOffset, contentSize} = e.nativeEvent;
-                    const distanceToBottom = contentSize.height - contentOffset.y - layoutMeasurement.height;
-                    if (distanceToBottom < SCROLL_DISTANCE_TO_LOAD) this.fetchMore();
+            <ThemeConsumer>
+                {({theme}: ThemeProps) => {
+                    const styles = themedStyles(theme);
+                    return (
+                        <ScrollView
+                            ref={this.scrollViewRef}
+                            style={styles.scroller}
+                            contentContainerStyle={[styles.itemsContainer, itemsContainerStyle]}
+                            refreshControl={
+                                <RefreshControl
+                                    progressViewOffset={progressViewOffset}
+                                    refreshing={fetching}
+                                    onRefresh={() => refresh()}
+                                />
+                            }
+                            onScroll={(e: NativeSyntheticEvent<NativeScrollEvent>) => {
+                                const {layoutMeasurement, contentOffset, contentSize} = e.nativeEvent;
+                                const distanceToBottom =
+                                    contentSize.height - contentOffset.y - layoutMeasurement.height;
+                                if (distanceToBottom < SCROLL_DISTANCE_TO_LOAD) this.fetchMore();
+                            }}
+                        >
+                            {items
+                                .filter((it: T) => !hiddenIds[id(it)])
+                                .map((it: T) => renderItem(it, () => this.hideItem(it)))}
+                            <View style={styles.loadingIndicatorContainer}>
+                                {fetching && items.length > 0 && (
+                                    <ActivityIndicator size="large" color={theme.accentSecondary} />
+                                )}
+                            </View>
+                            {!fetching && items.length == 0 && (
+                                <View style={styles.noResultsContainer}>{noResultsComponent}</View>
+                            )}
+                        </ScrollView>
+                    );
                 }}
-            >
-                <View style={styles.itemsContainer}>
-                    {items
-                        .filter((it: T) => !hiddenIds[id(it)])
-                        .map((it: T) => renderItem(it, () => this.hideItem(it)))}
-                    <View style={styles.loadingIndicatorContainer}>
-                        {fetching && items.length > 0 && (
-                            <ActivityIndicator size="large" color={theme.accentSecondary} />
-                        )}
-                    </View>
-                    {!fetching && items.length == 0 && (
-                        <View style={styles.noResultsContainer}>{noResultsComponent}</View>
-                    )}
-                </View>
-            </ScrollView>
+            </ThemeConsumer>
         );
     }
 }
@@ -108,10 +145,7 @@ const themedStyles = preTheme((/*theme: Theme*/) => {
             width: "100%",
         },
         itemsContainer: {
-            flex: 1,
-            borderStyle: "solid",
-            borderColor: "red",
-            borderWidth: 0,
+            //flex: 1,
         },
         loadingIndicatorContainer: {
             marginVertical: 10,
@@ -123,11 +157,3 @@ const themedStyles = preTheme((/*theme: Theme*/) => {
         },
     });
 });
-
-export default function InfiniteScroller<T>(props: InfiniteScrollerProps<T>): JSX.Element {
-    return (
-        <ThemeConsumer>
-            {(themeProps: ThemeProps) => <InfiniteScrollerClass {...props} {...themeProps} />}
-        </ThemeConsumer>
-    );
-}
