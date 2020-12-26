c4ed694bfaf6f7035e8f405ea5bd046b673dccdc
Kelian Baert <kelian@baert.me>
2020-12-26 11:46:13 +0100

    added const REPORT_TYPES

--- src/constants/reports.ts
+++ src/constants/reports.ts
@@ -2,11 +2,21 @@ export enum ReportType {
     VIOLENCE = "violence",
     NUDITY = "nudity",
     HARASSMENT = "harassment",
-    UNDERISABLE_CONTENT = "undesirable-content",
+    UNDESIRABLE_CONTENT = "undesirable-content",
     HATE_SPEECH = "hate-speech",
     VULGAR_CONTENT = "vulgar-content",
     OTHER = "other",
 }
+
+export const REPORT_TYPES = [
+    ReportType.VIOLENCE,
+    ReportType.NUDITY,
+    ReportType.HARASSMENT,
+    ReportType.UNDESIRABLE_CONTENT,
+    ReportType.HATE_SPEECH,
+    ReportType.VULGAR_CONTENT,
+    ReportType.OTHER,
+];
 
 export enum ReportEntityType {
     PROFILE_ENTITY = "ProfileEntity",
