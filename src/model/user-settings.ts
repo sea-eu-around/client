e987741437d76b537ed98c2d02aad46066db8170
Kelian Baert <kelian@baert.me>
2020-12-27 15:21:15 +0100

    added store actions for cookie consent

--- src/model/user-settings.ts
+++ src/model/user-settings.ts
@@ -1,7 +1,17 @@
 import {SupportedLocale} from "../localization";
 import {ThemeKey} from "../types";
 
+export type CookiesPreferences = {
+    auth: boolean;
+    cache: boolean;
+    settings: boolean;
+};
+
+export const COOKIES_PREFERENCES_KEYS = ["auth", "cache", "settings"];
+
 export type UserSettings = {
     theme: ThemeKey;
     locale: SupportedLocale;
+    cookies: CookiesPreferences;
+    cookieConsentDate: Date | null;
 };
--- src/state/settings/actions.ts
+++ src/state/settings/actions.ts
@@ -1,11 +1,15 @@
 import {ThemeKey} from "../../types";
 import {SupportedLocale} from "../../localization";
 import i18n from "i18n-js";
+import {CookiesPreferences} from "../../model/user-settings";
+import {AppThunk} from "../types";
 
 export enum SETTINGS_ACTION_TYPES {
     SET_THEME = "SETTINGS/SET_THEME",
     TOGGLE_THEME = "SETTINGS/TOGGLE_THEME",
     SET_LOCALE = "SETTINGS/SET_LOCALE",
+    LOAD_COOKIES_PREFERENCES = "SETTINGS/LOAD_COOKIES_PREFERENCES",
+    SAVE_COOKIES_PREFERENCES = "SETTINGS/SAVE_COOKIES_PREFERENCES",
 }
 
 export type SetThemeAction = {
@@ -24,7 +28,18 @@ export type SetLocaleAction = {
     fromCache?: boolean;
 };
 
-export type SettingsAction = SetThemeAction | SetLocaleAction;
+export type LoadCookiesPreferencesAction = {
+    type: string;
+    preferences: CookiesPreferences;
+    consentDate: Date;
+};
+
+export type SaveCookiesPreferencesAction = {
+    type: string;
+    preferences: CookiesPreferences;
+};
+
+export type SettingsAction = SetThemeAction | SetLocaleAction | LoadCookiesPreferencesAction;
 
 export const setTheme = (theme: ThemeKey, fromCache?: boolean): SetThemeAction =>
     ({type: SETTINGS_ACTION_TYPES.SET_THEME, theme, fromCache} as SetThemeAction);
@@ -34,4 +49,22 @@ export const toggleTheme = (): ToggleThemeAction => ({type: SETTINGS_ACTION_TYPE
 export const setLocale = (locale: SupportedLocale, fromCache?: boolean): SetLocaleAction => {
     i18n.locale = locale;
     return {type: SETTINGS_ACTION_TYPES.SET_LOCALE, locale, fromCache} as SetLocaleAction;
+};
+
+export const loadCookiesPreferences = (
+    preferences: CookiesPreferences,
+    consentDate: Date,
+): LoadCookiesPreferencesAction => ({
+    type: SETTINGS_ACTION_TYPES.LOAD_COOKIES_PREFERENCES,
+    preferences,
+    consentDate,
+});
+
+export const saveCookiesPreferences = (preferences: CookiesPreferences): SaveCookiesPreferencesAction => ({
+    type: SETTINGS_ACTION_TYPES.SAVE_COOKIES_PREFERENCES,
+    preferences,
+});
+
+export const acceptAllCookies = (): AppThunk => async (dispatch) => {
+    dispatch(saveCookiesPreferences({auth: true, cache: true, settings: true}));
 };
