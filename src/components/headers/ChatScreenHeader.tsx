82f40c9de610890abc1712549db521bae545479c
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2021-01-08 11:41:46 +0100

    improved rendering

--- src/components/headers/ChatScreenHeader.tsx
+++ src/components/headers/ChatScreenHeader.tsx
@@ -6,31 +6,59 @@ import {withTheme} from "react-native-elements";
 import {ThemeProps} from "../../types";
 import {preTheme} from "../../styles/utils";
 import {GiftedAvatar} from "react-native-gifted-chat";
-import {ChatRoomUser} from "../../model/chat-room";
+import {ChatRoom, ChatRoomUser} from "../../model/chat-room";
 import {headerStyles} from "../../styles/headers";
 import {rootNavigate} from "../../navigation/utils";
 import MainHeader, {MainHeaderStackProps} from "./MainHeader";
+import {RootNavigatorScreens} from "../../navigation/types";
+import {StackScreenProps} from "@react-navigation/stack";
+import store from "../../state/store";
+import {Route} from "@react-navigation/native";
 
 // Map props from store
 const reduxConnector = connect((state: AppState) => ({
-    activeRoom: state.messaging.activeRoom,
+    rooms: store.getState().messaging.matchRooms,
+    activeRoom: store.getState().messaging.activeRoom,
     profileId: state.profile.user?.profile?.id,
 }));
 
 // Component props
-export type ChatScreenHeaderProps = ConnectedProps<typeof reduxConnector> & ThemeProps & MainHeaderStackProps;
+export type ChatScreenHeaderProps = ConnectedProps<typeof reduxConnector> &
+    ThemeProps &
+    StackScreenProps<RootNavigatorScreens> &
+    Omit<MainHeaderStackProps, "route">;
 
 class ChatScreenHeaderClass extends React.Component<ChatScreenHeaderProps> {
+    private getRoomId(): string | null {
+        const {route} = this.props;
+        // Get the room ID from the route parameters
+        if (route.params) {
+            const params = route.params as {[key: string]: string};
+            const {roomId} = params;
+            return roomId;
+        }
+        return null;
+    }
+
+    private getRoom(): ChatRoom | null {
+        const {rooms, activeRoom} = this.props;
+        const roomId = this.getRoomId();
+        return activeRoom || (roomId ? rooms[roomId] : null);
+    }
+
     render(): JSX.Element {
-        const {theme, activeRoom, profileId, ...stackProps} = this.props;
+        const {theme, profileId, ...stackProps} = this.props;
         const styles = themedStyles(theme);
         const hstyles = headerStyles(theme);
 
-        if (activeRoom) {
-            const user = activeRoom.users.filter((p: ChatRoomUser) => p._id != profileId)[0];
+        const room = this.getRoom();
+
+        if (room) {
+            const user = room.users.filter((p: ChatRoomUser) => p._id != profileId)[0];
             return (
                 <MainHeader
                     {...stackProps}
+                    route={stackProps.route as Route<string, undefined>}
                     backButton={true}
                     blur={true}
                     overrideAvatar={
@@ -59,6 +87,8 @@ const themedStyles = preTheme(() => {
 
 const ChatScreenHeaderComp = reduxConnector(withTheme(ChatScreenHeaderClass));
 
-export default function ChatScreenHeader(props: MainHeaderStackProps): JSX.Element {
+export default function ChatScreenHeader(
+    props: StackScreenProps<RootNavigatorScreens> & MainHeaderStackProps,
+): JSX.Element {
     return <ChatScreenHeaderComp {...props} />;
 }
