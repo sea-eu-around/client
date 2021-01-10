aaa3c8253a38760cb379acb17ae93ce8f4bc1008
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2021-01-10 14:39:46 +0100

    updated blur

--- src/components/modals/ModalImpl.native.tsx
+++ src/components/modals/ModalImpl.native.tsx
@@ -96,11 +96,7 @@ class ModalImpl extends React.Component<ModalImplProps, ModalImplState> {
         return (
             <Modal animationType={animationType} transparent={true} statusBarTranslucent={true} visible={modalVisible}>
                 {backdropBlur ? (
-                    <BlurView
-                        style={{flex: 1}}
-                        tint={theme.id === "dark" ? "dark" : "default"}
-                        intensity={BLUR_MODAL_INTENSITY}
-                    >
+                    <BlurView style={{flex: 1}} tint={"dark"} intensity={BLUR_MODAL_INTENSITY}>
                         {content}
                     </BlurView>
                 ) : (
