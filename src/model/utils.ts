1d4ac0008a6279886ec0286d45c63a272700126e
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2021-01-10 15:51:18 +0100

    added utility function to get matching offers between two profiles

--- src/model/utils.ts
+++ src/model/utils.ts
@@ -1,3 +1,6 @@
+import {OfferValueDto} from "../api/dto";
+import {UserProfile} from "./user-profile";
+
 /**
  * Attempts to extract names from a given email.
  * @param email A valid email address.
@@ -12,4 +15,24 @@ export function extractNamesFromEmail(email: string): {firstname: string; lastna
               lastname: capitalize(splitName[1]),
           }
         : null;
+}
+
+/**
+ * Filter only the offers that match a given profile.
+ * @param offers A list of offer values.
+ * @param profile A profile
+ * @returns the offers that target the given profile.
+ */
+export function getMatchingOffers(offers: OfferValueDto[], profile: UserProfile): OfferValueDto[] {
+    return offers.filter((o: OfferValueDto) => {
+        if (
+            (!o.allowFemale && profile.gender === "female") ||
+            (!o.allowMale && profile.gender === "male") ||
+            (!o.allowOther && profile.gender === "other") ||
+            (!o.allowStaff && profile.type === "staff") ||
+            (!o.allowStudent && profile.type === "student")
+        )
+            return false;
+        return true;
+    });
 }
