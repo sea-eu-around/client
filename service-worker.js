f3c50c3fa944f2e3c6a93a22c8771ae81c360c88
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2021-01-19 16:29:20 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bd62fa25f71f8fedb3613fb6521b7a9f.js"
+  "/precache-manifest.5e941f4cb8db72b2ead5fe41741a1c73.js"
 );
 
 workbox.skipWaiting();
--- static/js/13.adc5cf6f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonp=this.webpackJsonp||[]).push([[13],{768:function(s,e,p){s.exports=p.p+"static/media/logosbeneficaireserasmusleft_fr_0.a62f8d2d.png"}}]);
-//# sourceMappingURL=13.adc5cf6f.chunk.js.map
