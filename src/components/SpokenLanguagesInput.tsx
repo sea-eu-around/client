f37260aa40c1786f74f9ba6d0d843b599bc7d10f
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2021-02-01 16:43:58 +0100

    added buttonStyleVariant for level picker

--- src/components/SpokenLanguagesInput.tsx
+++ src/components/SpokenLanguagesInput.tsx
@@ -88,6 +88,7 @@ class SpokenLanguagesInput extends React.Component<SpokenLanguagesInputProps, Sp
                         level={sl.level}
                         buttonStyle={styles.levelPicker}
                         onChange={(level: LanguageLevel) => this.setLanguageLevel(i, level)}
+                        buttonStyleVariant={pickerButtonStyleVariant}
                     />
                     <TouchableOpacity style={styles.deleteIconContainer} onPress={() => this.removeRow(i)}>
                         <MaterialIcons style={styles.deleteIcon} name="close" />
