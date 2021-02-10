1fa492a69ef8dff60b31a168030ea531975026c7
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2021-02-10 19:12:29 +0100

    added textStyle prop

--- src/components/forms/FormError.tsx
+++ src/components/forms/FormError.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Text, View, ViewProps, StyleSheet} from "react-native";
+import {Text, View, ViewProps, StyleSheet, StyleProp, TextStyle} from "react-native";
 import {withTheme} from "react-native-elements";
 import {preTheme} from "../../styles/utils";
 import {Theme, ThemeProps} from "../../types";
@@ -8,18 +8,19 @@ import {localizeError} from "../../api/errors";
 // Component props
 export type FormErrorProps = ThemeProps & {
     error?: string;
+    textStyle?: StyleProp<TextStyle>;
 } & ViewProps;
 
 class FormError extends React.Component<FormErrorProps> {
     render(): JSX.Element {
-        const {error, theme, style, ...otherProps} = this.props;
+        const {error, theme, style, textStyle, ...otherProps} = this.props;
         const styles = themedStyles(theme);
 
         return (
             <>
                 {error && error.length > 0 && (
                     <View style={[styles.wrapper, style]} {...otherProps}>
-                        <Text style={styles.text}>{error ? localizeError(error) : ""}</Text>
+                        <Text style={[styles.text, textStyle]}>{localizeError(error)}</Text>
                     </View>
                 )}
             </>
