46c56aa2fdc9f1b98cbecd7ba169ebfdda681ddc
KelianB-IMTA <kelian@baert.me>
2021-02-18 12:00:07 +0100

    added arrayWithIdMapperToDict utility function

--- src/general-utils.ts
+++ src/general-utils.ts
@@ -1,3 +1,7 @@
 export function arrayWithIdsToDict<T extends {id: string}>(arr: T[]): {[key: string]: T} {
     return arr.reduce((dict, el) => ((dict[el.id] = el), dict), {} as {[key: string]: T});
 }
+
+export function arrayWithIdMapperToDict<T>(arr: T[], id: (item: T) => string): {[key: string]: T} {
+    return arr.reduce((dict, el) => ((dict[id(el)] = el), dict), {} as {[key: string]: T});
+}
