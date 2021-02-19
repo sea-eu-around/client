1c413d5ff85b5383de7c2dab78a6a3275203f4d3
KelianB-IMTA <kelian@baert.me>
2021-02-19 14:49:40 +0100

    added various modals to confirm group actions

---
+++ src/components/modals/DeleteGroupMemberModal.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import i18n from "i18n-js";
+import {ThemeProps} from "../../types";
+import {withTheme} from "react-native-elements";
+import {UserProfile} from "../../model/user-profile";
+import {MyThunkDispatch} from "../../state/types";
+import store from "../../state/store";
+import {CustomModalClass, CustomModalProps} from "./CustomModal";
+import ConfirmationModal from "./ConfirmationModal";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
+import {deleteGroupMember} from "../../state/groups/actions";
+
+export type DeleteGroupMemberModalProps = ThemeProps &
+    Partial<CustomModalProps> & {groupId: string; profile: UserProfile | null; pending?: boolean};
+
+export class DeleteGroupMemberModalClass extends React.Component<DeleteGroupMemberModalProps> {
+    modalRef = React.createRef<CustomModalClass>();
+
+    show(): void {
+        this.modalRef.current?.show();
+    }
+
+    render(): JSX.Element {
+        const {theme, groupId, profile, pending, ...otherProps} = this.props;
+        const dispatch = store.dispatch as MyThunkDispatch;
+
+        const translationRoot = pending ? "groups.deletePendingMember" : "groups.deleteMember";
+
+        return (
+            <ConfirmationModal
+                title={i18n.t(`${translationRoot}.title`)}
+                text={
+                    profile ? i18n.t(`${translationRoot}.text`, {name: `${profile.firstName} ${profile.lastName}`}) : ""
+                }
+                icon={(props) => <MaterialCommunityIcons name="account-remove" color={theme.error} {...props} />}
+                buttons={[
+                    {preset: "cancel"},
+                    {
+                        preset: "delete",
+                        text: i18n.t(`${translationRoot}.action`),
+                        onPress: async (hide) => {
+                            if (profile) {
+                                const success = await dispatch(deleteGroupMember(groupId, profile.id));
+                                if (success) hide();
+                            }
+                        },
+                    },
+                ]}
+                {...otherProps}
+            />
+        );
+    }
+}
+
+export default withTheme(DeleteGroupMemberModalClass);
--- src/components/modals/DeletePostConfirmModal.tsx
+++ src/components/modals/DeletePostConfirmModal.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {ThemeProps} from "../../types";
 import {withTheme} from "react-native-elements";
 import {CustomModalProps} from "./CustomModal";
-import ConfirmationModal from "./ConfirmationModal";
+import ConfirmationModal, {ConfirmationModalClass} from "./ConfirmationModal";
 import {FontAwesome} from "@expo/vector-icons";
 import i18n from "i18n-js";
 import store from "../../state/store";
@@ -12,12 +12,19 @@ import {MyThunkDispatch} from "../../state/types";
 
 export type DeletePostConfirmModalProps = ThemeProps & Partial<CustomModalProps> & {groupId: string; post: GroupPost};
 
-class DeletePostConfirmModal extends React.Component<DeletePostConfirmModalProps> {
-    render() {
+export class DeletePostConfirmModalClass extends React.Component<DeletePostConfirmModalProps> {
+    private modalRef = React.createRef<ConfirmationModalClass>();
+
+    show(): void {
+        this.modalRef.current?.show();
+    }
+
+    render(): JSX.Element {
         const {groupId, post, theme, ...otherProps} = this.props;
 
         return (
             <ConfirmationModal
+                ref={this.modalRef}
                 title={i18n.t("groups.deletePost.title")}
                 text={i18n.t("groups.deletePost.text")}
                 icon={(props) => <FontAwesome name="warning" color={theme.error} {...props} />}
@@ -37,4 +44,4 @@ class DeletePostConfirmModal extends React.Component<DeletePostConfirmModalProps
     }
 }
 
-export default withTheme(DeletePostConfirmModal);
+export default withTheme(DeletePostConfirmModalClass);
