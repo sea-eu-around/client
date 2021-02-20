5277399612d2dfb5df58755100b4bdd93fa3eca7
KelianB-IMTA <kelian@baert.me>
2021-02-20 13:56:24 +0100

    added group settings menu

--- src/components/headers/GroupScreenHeader.tsx
+++ src/components/headers/GroupScreenHeader.tsx
@@ -7,6 +7,7 @@ import {withTheme} from "react-native-elements";
 import {ThemeProps} from "../../types";
 import {connect, ConnectedProps} from "react-redux";
 import {AppState} from "../../state/types";
+import GroupSettingsMenu, {GroupSettingsMenuClass} from "../GroupSettingsMenu";
 
 const reduxConnector = connect((state: AppState) => ({
     groupsDict: state.groups.groupsDict,
@@ -20,6 +21,8 @@ export type GroupScreenHeaderProps = MainHeaderStackProps &
     };
 
 class GroupScreenHeaderClass extends React.Component<GroupScreenHeaderProps> {
+    settingsMenuRef = React.createRef<GroupSettingsMenuClass>();
+
     private getGroup(): Group | null {
         const {groupsDict, groupId} = this.props;
         return groupId ? groupsDict[groupId] || null : null;
@@ -28,33 +31,48 @@ class GroupScreenHeaderClass extends React.Component<GroupScreenHeaderProps> {
     render(): JSX.Element {
         const {theme, ...stackProps} = this.props;
 
-        // const group = this.getGroup();
+        const group = this.getGroup();
 
         return (
-            <MainHeader
-                {...stackProps}
-                backButton
-                noAvatar
-                noShadow
-                noSettingsButton
-                wrapperStyle={{position: "absolute", backgroundColor: "transparent", top: 0, left: 0, right: 0}}
-                color={theme.textWhite}
-                buttonBackgroundColor="transparent"
-                // overrideTitle={group ? group.name : ""}
-                overrideTitle=""
-                rightButtons={[
-                    ({buttonStyle, iconStyle}) => (
-                        <TouchableOpacity style={buttonStyle} /*onPress={() => rootNavigate("MatchHistoryScreen")}*/>
-                            <MaterialIcons name="search" style={iconStyle} />
-                        </TouchableOpacity>
-                    ),
-                    ({buttonStyle, iconStyle}) => (
-                        <TouchableOpacity style={buttonStyle} /*onPress={() => rootNavigate("MatchHistoryScreen")}*/>
-                            <MaterialCommunityIcons name="dots-horizontal" style={[iconStyle, {fontSize: 30}]} />
-                        </TouchableOpacity>
-                    ),
-                ]}
-            />
+            <>
+                <MainHeader
+                    {...stackProps}
+                    backButton
+                    noAvatar
+                    noShadow
+                    noSettingsButton
+                    wrapperStyle={{position: "absolute", backgroundColor: "transparent", top: 0, left: 0, right: 0}}
+                    color={theme.textWhite}
+                    buttonBackgroundColor="transparent"
+                    // overrideTitle={group ? group.name : ""}
+                    overrideTitle=""
+                    rightButtons={
+                        group
+                            ? [
+                                  ({buttonStyle, iconStyle}) => (
+                                      <TouchableOpacity
+                                          style={buttonStyle} /*onPress={() => rootNavigate("MatchHistoryScreen")}*/
+                                      >
+                                          <MaterialIcons name="search" style={iconStyle} />
+                                      </TouchableOpacity>
+                                  ),
+                                  ({buttonStyle, iconStyle}) => (
+                                      <TouchableOpacity
+                                          style={buttonStyle}
+                                          onPress={() => this.settingsMenuRef.current?.show(group)}
+                                      >
+                                          <MaterialCommunityIcons
+                                              name="dots-horizontal"
+                                              style={[iconStyle, {fontSize: 30}]}
+                                          />
+                                      </TouchableOpacity>
+                                  ),
+                              ]
+                            : []
+                    }
+                />
+                <GroupSettingsMenu ref={this.settingsMenuRef} />
+            </>
         );
     }
 }
