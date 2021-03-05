468724a7282074741614b7e27c4c5e8be1438fa1
KelianB-IMTA <kelian@baert.me>
2021-03-05 14:01:11 +0100

    added utility to make promises cancellable

--- src/general-utils.ts
+++ src/general-utils.ts
@@ -5,3 +5,18 @@ export function arrayWithIdsToDict<T extends {id: string}>(arr: T[]): {[key: str
 export function arrayWithIdMapperToDict<T>(arr: T[], id: (item: T) => string): {[key: string]: T} {
     return arr.reduce((dict, el) => ((dict[id(el)] = el), dict), {} as {[key: string]: T});
 }
+
+export type CancelablePromise<T = unknown> = Promise<T> & {cancel: () => void};
+
+export function makePromiseCancelable<T>(promise: Promise<T>): CancelablePromise<T> {
+    let hasCanceled = false;
+
+    const wrappedPromise = new Promise<T>((resolve, reject) => {
+        promise.then((val: T) => (hasCanceled ? reject({canceled: true}) : resolve(val)));
+        promise.catch((error) => (hasCanceled ? reject({canceled: true}) : reject(error)));
+    });
+
+    const cancel = () => (hasCanceled = true);
+
+    return Object.assign(wrappedPromise, {cancel});
+}
