c0b124eda899bdd683b774f0bf4ceb72534eab27
KelianB-IMTA <kelian@baert.me>
2021-03-09 10:45:57 +0100

    bumped version

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 const TARGET = process.env.TARGET || "STAGING";
 
-const VERSION = "1.0.4";
-const ANDROID_VERSION_CODE = 14;
+const VERSION = "1.1.0";
+const ANDROID_VERSION_CODE = 15;
 
 const CLIENT_HTTP = "https";
 
